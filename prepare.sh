116ee77fef9920c1c765714e781761af5cc1208f
Christopher Thompson <chrislegolife@gmail.com>
2015-04-26 17:47:50 -0400

    Added sass support and changed main styling of site to Materialize

--- prepare.sh
+++ prepare.sh
@@ -2,3 +2,11 @@
 
 sudo apt-get install build-essential python-dev python-pip libffi-dev
 pip install -r requirements.txt
+
+# Requirements for SASS
+sudo apt-get install ruby -y
+sudo gem install sass
+
+# Start watching for changes to SASS
+mkdir log
+nohup sass --watch static/sass/materialize.scss:static/css/materialize.css > log/$(/bin/date +%Y%m%d.%H%M).sass_watch.log 2>&1 &
