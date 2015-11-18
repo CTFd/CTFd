81345dc6d2ca774ae92e01dd4a6658cafbd8b532
CodeKevin <kevin@codekevin.com>
2015-11-17 19:46:44 -0500

    Auto-generate secret key and fixing challenge modal

--- CTFd/config.py
+++ CTFd/config.py
@@ -1,6 +1,16 @@
 import os
+
+##### GENERATE SECRET KEY #####
+with open('.ctfd_secret_key', 'a+') as secret:
+    secret.seek(0)  # Seek to beginning of file since a+ mode leaves you at the end and w+ deletes the file
+    key = secret.read()
+    if not key:
+        key = os.urandom(64)
+        secret.write(key)
+        secret.flush()
+
 ##### SERVER SETTINGS #####
-SECRET_KEY = os.urandom(64)
+SECRET_KEY = key
 SQLALCHEMY_DATABASE_URI = 'sqlite:///ctfd.db'
 SESSION_TYPE = "filesystem"
 SESSION_FILE_DIR = "/tmp/flask_session"
