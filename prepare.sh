540c2c88a024a106103f5b8271b33344ffd0777d
Alexandre ZANNI <noraj1337@users.noreply.github.com>
2017-02-02 23:49:18 +0100

    menu to select OS

--- prepare.sh
+++ prepare.sh
@@ -1,4 +1,23 @@
 #!/bin/sh
 
-sudo apt-get install build-essential python-dev python-pip libffi-dev -y
-pip install -r requirements.txt
+PS3='Please select your OS: '
+options=("Debian" "ArchLinux" "Quit")
+select opt in "${options[@]}"
+do
+    case $opt in
+        "Debian")
+            sudo apt-get install build-essential python-dev python-pip libffi-dev -y
+	    pip install -r requirements.txt
+	    break
+            ;;
+        "ArchLinux")
+            sudo pacman -S --needed --noconfirm base-devel python-pip libffi
+            pip2 install --user -r requirements.txt
+	    break
+            ;;
+        "Quit")
+            break
+            ;;
+        *) echo invalid option;;
+    esac
+done
