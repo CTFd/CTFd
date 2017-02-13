325abfdc20ae5dd20b9f494850993a34669ac707
Eric Rasche <rasche.eric@gmail.com>
2017-02-13 02:53:49 +0000

    Wait on mysql to be alive before trying to connect

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM python:2.7-alpine
 RUN apk update && \
-    apk add python python-dev libffi-dev gcc make musl-dev py-pip
+    apk add python python-dev libffi-dev gcc make musl-dev py-pip mysql-client
 
 RUN mkdir -p /opt/CTFd
 COPY . /opt/CTFd
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,6 +2,17 @@
 # First, if a yaml file is passed in via environment variable, we
 # auto-poulate from that.
 
+# http://stackoverflow.com/questions/25503412/how-do-i-know-when-my-docker-mysql-container-is-up-and-mysql-is-ready-for-taking#29793382
+echo "Waiting on MySQL"
+while ! mysqladmin ping -h db --silent; do
+	# Show some progress
+	echo -n '.';
+	sleep 1;
+done
+echo "Ready"
+# Give it another second.
+sleep 1;
+
 if [ ! -z "$CTFD_YAML_FILE" ]; then
 	if [ -e "$CTFD_YAML_FILE" ]; then
 		python populate-yaml.py $CTFD_YAML_FILE;
