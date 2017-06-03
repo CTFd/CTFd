a833e8514ca83030f09c214a21125299cecad10d
Kevin Chung <kchung@nyu.edu>
2017-06-03 17:43:46 -0400

    Remove --threads (#268)
    Should fix #266

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -12,4 +12,4 @@ echo "Ready"
 sleep 1;
 
 echo "Starting CTFd"
-gunicorn --bind 0.0.0.0:8000 -w 1 --threads 8 'CTFd:create_app()' --access-logfile '/opt/CTFd/CTFd/logs/access.log' --error-logfile '/opt/CTFd/CTFd/logs/error.log'
+gunicorn --bind 0.0.0.0:8000 -w 1 'CTFd:create_app()' --access-logfile '/opt/CTFd/CTFd/logs/access.log' --error-logfile '/opt/CTFd/CTFd/logs/error.log'
