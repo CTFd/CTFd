aea3ce50427ad054d49ce3e087f89384f6942a60
Kevin Chung <kchung@nyu.edu>
2017-06-16 16:53:28 -0400

    Only apply sqlite hacks for sqlite
    I don’t think this is really necessary but if Sqlite is the one with
    the problem there’s no point in doing additional logic for every db.

--- CTFd/utils.py
+++ CTFd/utils.py
@@ -793,13 +793,14 @@ def import_ctf(backup, segments=None, erase=False):
                 for entry in saved['results']:
                     entry_id = entry.pop('id', None)
                     # This is a hack to get SQlite to properly accept datetime values from dataset
-                    for k, v in entry.items():
-                        if isinstance(v, six.string_types):
-                            try:
-                                entry[k] = datetime.datetime.strptime(v, '%Y-%m-%dT%H:%M:%S')
-                            except ValueError as e:
-                                print(e)
-                                pass
+                    if get_config('SQLALCHEMY_DATABASE_URI').startswith('sqlite'):
+                        for k, v in entry.items():
+                            if isinstance(v, six.string_types):
+                                try:
+                                    entry[k] = datetime.datetime.strptime(v, '%Y-%m-%dT%H:%M:%S')
+                                except ValueError as e:
+                                    print(e)
+                                    pass
                     table.insert(entry)
             else:
                 continue
