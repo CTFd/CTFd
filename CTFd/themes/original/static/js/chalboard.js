4862a3ca10a6ba0defb3b8d19f111f433786fd2f
Kevin Chung <kchung@nyu.edu>
2017-10-05 23:14:36 -0400

    Fixing subdirectory deployment (#401)
    * Fixing subdirectory deploy linking incorrectly (Closes #400)
    * Fix some admin panel CSS

--- CTFd/themes/original/static/js/chalboard.js
+++ CTFd/themes/original/static/js/chalboard.js
@@ -195,7 +195,7 @@ function getsolves(id){
       var id = teams[i].id;
       var name = teams[i].name;
       var date = moment(teams[i].date).local().format('LLL');
-      box.append('<tr><td><a href="/team/{0}">{1}</td><td>{2}</td></tr>'.format(id, htmlentities(name), date));
+      box.append('<tr><td><a href="team/{0}">{1}</td><td>{2}</td></tr>'.format(id, htmlentities(name), date));
     };
   });
 }
