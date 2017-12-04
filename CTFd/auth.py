80ee2c7940c73f1e77b091a3b111faeb2107a201
Kevin Chung <kchung@nyu.edu>
2017-12-04 18:07:41 -0500

    Relax ratelimiting

--- CTFd/auth.py
+++ CTFd/auth.py
@@ -16,7 +16,7 @@ auth = Blueprint('auth', __name__)
 
 @auth.route('/confirm', methods=['POST', 'GET'])
 @auth.route('/confirm/<data>', methods=['GET'])
-@ratelimit(limit=5, interval=60)
+@ratelimit(limit=20, interval=60)
 def confirm_user(data=None):
     if not utils.get_config('verify_emails'):
         # If the CTF doesn't care about confirming email addresses then redierct to challenges
@@ -77,7 +77,7 @@ def confirm_user(data=None):
 
 @auth.route('/reset_password', methods=['POST', 'GET'])
 @auth.route('/reset_password/<data>', methods=['POST', 'GET'])
-@ratelimit(limit=5, interval=60)
+@ratelimit(limit=20, interval=60)
 def reset_password(data=None):
     logger = logging.getLogger('logins')
     if data is not None and request.method == "GET":
@@ -137,7 +137,7 @@ Did you initiate a password reset?
 
 
 @auth.route('/register', methods=['POST', 'GET'])
-@ratelimit(limit=10, interval=60)
+@ratelimit(limit=150, interval=60)
 def register():
     logger = logging.getLogger('regs')
     if not utils.can_register():
@@ -213,7 +213,7 @@ def register():
 
 
 @auth.route('/login', methods=['POST', 'GET'])
-@ratelimit(limit=10, interval=60)
+@ratelimit(limit=150, interval=60)
 def login():
     logger = logging.getLogger('logins')
     if request.method == 'POST':
