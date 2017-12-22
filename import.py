f11b5985d3fd9c016a705cdf65fcda8855db1eb2
Kevin Chung <kchung@nyu.edu>
2017-12-21 23:34:27 -0500

    Choose segments with import.py

--- import.py
+++ import.py
@@ -1,9 +1,16 @@
+"""
+python import.py export.zip challenges,teams,both,metadata
+"""
 from CTFd import create_app
 from CTFd.utils import import_ctf
 
-import zipfile
 import sys
 
 app = create_app()
 with app.app_context():
-    import_ctf(sys.argv[1])
+    if sys.argv[2]:
+        segments = sys.argv[2].split(',')
+    else:
+        segments = None
+
+    import_ctf(sys.argv[1], segments=segments)
