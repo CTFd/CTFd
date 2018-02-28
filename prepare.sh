3210b32c7fec889ffb1c6bff219f0845a05992a2
appleorange1 <appleorange1@users.noreply.github.com>
2018-02-28 12:21:31 +1100

    Fixes for installation procedure - add libssl-dev, remove dependency on sudo

--- prepare.sh
+++ prepare.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-sudo apt-get update
-sudo apt-get install build-essential python-dev python-pip libffi-dev -y
+apt-get update
+apt-get install build-essential python-dev python-pip libffi-dev libssl-dev -y
+pip install 
 pip install -r requirements.txt
