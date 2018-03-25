ca8f4388424d2dea7240d7a3031dd1f5c63de4b8
Kevin Chung <kchung@nyu.edu>
2018-03-25 02:16:48 -0400

    Add workers & missing SECRET_KEY error

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
 
+if [ ! -f .ctfd_secret_key ] && [ -z "$SECRET_KEY" ]; then
+    if [ $WORKERS -gt 1 ]; then
+        echo "[ ERROR ] You are configured to use more than 1 worker."
+        echo "[ ERROR ] To do this, you must define the SECRET_KEY environment variable or create a .ctfd_secret_key file."
+        echo "[ ERROR ] Exiting..."
+        exit 1
+    fi
+fi
+
+
 if [ -n "$DATABASE_URL" ]
     then
     # https://stackoverflow.com/a/29793382
@@ -17,7 +27,7 @@ fi
 echo "Starting CTFd"
 gunicorn 'CTFd:create_app()' \
     --bind '0.0.0.0:8000' \
-    --workers 4 \
+    --workers $WORKERS \
     --worker-class 'gevent' \
     --access-logfile "${LOG_FOLDER:-/opt/CTFd/CTFd/logs}/access.log" \
     --error-logfile "${LOG_FOLDER:-/opt/CTFd/CTFd/logs}/error.log"
