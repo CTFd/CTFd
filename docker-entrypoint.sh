b78589d20de4abf97067c3b1c1cbfe72b498f616
Kevin Chung <kchung@nyu.edu>
2018-03-25 04:25:33 -0400

    Update docker files (#598)
    * Add redis to docker-compose, bump worker count to 4
    * Add workers & missing SECRET_KEY error
    * Remove uwsgi specific files
    * Parse database host in `docker-entrypoint.sh`. Closes #587

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,23 +1,35 @@
 #!/bin/sh
 
+# Check that a .ctfd_secret_key file or SECRET_KEY envvar is set
+if [ ! -f .ctfd_secret_key ] && [ -z "$SECRET_KEY" ]; then
+    if [ $WORKERS -gt 1 ]; then
+        echo "[ ERROR ] You are configured to use more than 1 worker."
+        echo "[ ERROR ] To do this, you must define the SECRET_KEY environment variable or create a .ctfd_secret_key file."
+        echo "[ ERROR ] Exiting..."
+        exit 1
+    fi
+fi
+
+# Check that the database is available
 if [ -n "$DATABASE_URL" ]
     then
-    # https://stackoverflow.com/a/29793382
-    echo "Waiting on MySQL"
-    while ! mysqladmin ping -h db --silent; do
+    database=`echo $DATABASE_URL | awk -F[@//] '{print $4}'`
+    echo "Waiting for $database to be ready"
+    while ! mysqladmin ping -h $database --silent; do
         # Show some progress
         echo -n '.';
         sleep 1;
     done
-    echo "Ready"
+    echo "$database is ready"
     # Give it another second.
     sleep 1;
 fi
 
+# Start CTFd
 echo "Starting CTFd"
 gunicorn 'CTFd:create_app()' \
     --bind '0.0.0.0:8000' \
-    --workers 1 \
+    --workers $WORKERS \
     --worker-class 'gevent' \
     --access-logfile "${LOG_FOLDER:-/opt/CTFd/CTFd/logs}/access.log" \
     --error-logfile "${LOG_FOLDER:-/opt/CTFd/CTFd/logs}/error.log"
