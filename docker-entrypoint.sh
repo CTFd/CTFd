9285b69bb0f991286a1fa62087c7215b2d3676d5
cooldup <cooldup@qq.com>
2018-04-12 10:19:29 +0800

    更新

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,18 +1,16 @@
 #!/bin/sh
 
 # The number of worker is recommended for the current CPU is  CPU number * 2 + 1 eg:2 CPU is 5
-# so you have create a  a .ctfd_secret_key file or set SECRET_KEY envvar
 WORKERS=5
 
-# Check that a .ctfd_secret_key file or SECRET_KEY envvar is set
-if [ ! -f .ctfd_secret_key ] && [ -z "$SECRET_KEY" ]; then
+# Check that a CTFd/.ctfd_secret_key file or SECRET_KEY envvar is set
+if [ ! -f CTFd/.ctfd_secret_key ] && [ -z "$SECRET_KEY" ]; then
     if [ $WORKERS -gt 1 ]; then
         echo "[ WARNING ] You are configured to use more than 1 worker."
-        echo "[ WARNING ] To do this, you must define the SECRET_KEY environment variable or create a .ctfd_secret_key file."
+        echo "[ WARNING ] To do this, you must define the SECRET_KEY environment variable or create a CTFd/.ctfd_secret_key file."
         echo "[ WARNING ] WORKERS will set to 1"
         WORKERS=1
     fi
- 
 fi
 
 # Check that the database is available
