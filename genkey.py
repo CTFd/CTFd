e2a6175681dacc217e79b8c455775240f57cdd1e
cooldup <cooldup@qq.com>
2018-04-12 11:18:31 +0800

    修正

--- genkey.py
+++ genkey.py
@@ -1,11 +1,12 @@
 import os
 ''' AUTO GENERATE SECRET KEY '''
 key = os.urandom(64)
-    try:
-        with open('CTFd/.ctfd_secret_key', 'wb') as secret:
-            secret.write(key)
-            secret.flush()
-            secret.close()
-            print 'Generate Succeed'
-    except (OSError, IOError):
-            print 'Generate Failed'
+try:
+  with open('CTFd/.ctfd_secret_key', 'wb') as secret:
+    secret.write(key)
+    secret.flush()
+    secret.close()
+    print key.encode('hex')
+    print 'Generate Succeed'
+except (OSError, IOError):
+  print 'Generate Failed'
