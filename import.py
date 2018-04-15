1fe392847d95c90f55dd3b1c7129327dd5ebc52e
Olivier Bilodeau <olivier@bottomlesspit.org>
2018-04-15 15:52:40 -0400

    import.py: Proper check for second argument

--- import.py
+++ import.py
@@ -8,7 +8,7 @@ import sys
 
 app = create_app()
 with app.app_context():
-    if sys.argv[2]:
+    if len(sys.argv) == 2:
         segments = sys.argv[2].split(',')
     else:
         segments = None
