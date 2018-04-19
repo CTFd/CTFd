9f321b0c36ee668855975ec218017acca5192731
Kevin Chung <kchung@nyu.edu>
2018-04-19 13:53:32 -0400

    Add window.challenge.submit, renderSubmissionResponse, and csrf_nonce

--- CTFd/plugins/challenges/assets/standard-challenge-modal.js
+++ CTFd/plugins/challenges/assets/standard-challenge-modal.js
@@ -14,3 +14,17 @@ window.challenge.render = function(markdown){
 window.challenge.postRender = function(){
 
 };
+
+
+window.challenge.submit = function(cb){
+    var chal_id = $('#chal-id').val();
+    var answer = $('#answer-input').val();
+    var nonce = $('#nonce').val();
+
+    $.post(script_root + "/chal/" + chal_id, {
+        key: answer,
+        nonce: nonce
+    }, function (data) {
+        cb(data);
+    });
+};
--- CTFd/themes/core/static/js/chalboard.js
+++ CTFd/themes/core/static/js/chalboard.js
@@ -59,10 +59,13 @@ function updateChalWindow(obj) {
                     $("#too-fast").slideUp();
                 });
 
-                $('#submit-key').unbind('click');
                 $('#submit-key').click(function (e) {
                     e.preventDefault();
-                    submitkey($('#chal-id').val(), $('#answer-input').val(), $('#nonce').val())
+                    $('#submit-key').addClass("disabled-button");
+                    $('#submit-key').prop('disabled', true);
+                    window.challenge.submit(function (data) {
+                        renderSubmissionResponse(data)
+                    });
                 });
 
                 $("#answer-input").keyup(function (event) {
@@ -101,72 +104,65 @@ $("#answer-input").keyup(function(event){
 });
 
 
-function submitkey(chal, key, nonce, cb) {
-    $('#submit-key').addClass("disabled-button");
-    $('#submit-key').prop('disabled', true);
-    $.post(script_root + "/chal/" + chal, {
-        key: key,
-        nonce: nonce
-    }, function (data) {
-        var result = $.parseJSON(JSON.stringify(data));
+function renderSubmissionResponse(data, cb){
+    var result = $.parseJSON(JSON.stringify(data));
 
-        var result_message = $('#result-message');
-        var result_notification = $('#result-notification');
-        var answer_input = $("#answer-input");
-        result_notification.removeClass();
-        result_message.text(result.message);
+    var result_message = $('#result-message');
+    var result_notification = $('#result-notification');
+    var answer_input = $("#answer-input");
+    result_notification.removeClass();
+    result_message.text(result.message);
 
-        if (result.status == -1){
-          window.location = script_root + "/login?next=" + script_root + window.location.pathname + window.location.hash
-          return
-        }
-        else if (result.status == 0){ // Incorrect key
-            result_notification.addClass('alert alert-danger alert-dismissable text-center');
-            result_notification.slideDown();
+    if (result.status == -1) {
+        window.location = script_root + "/login?next=" + script_root + window.location.pathname + window.location.hash
+        return
+    }
+    else if (result.status == 0) { // Incorrect key
+        result_notification.addClass('alert alert-danger alert-dismissable text-center');
+        result_notification.slideDown();
 
-            answer_input.removeClass("correct");
-            answer_input.addClass("wrong");
-            setTimeout(function () {
-                answer_input.removeClass("wrong");
-            }, 3000);
-        }
-        else if (result.status == 1){ // Challenge Solved
-            result_notification.addClass('alert alert-success alert-dismissable text-center');
-            result_notification.slideDown();
-
-            $('.chal-solves').text((parseInt($('.chal-solves').text().split(" ")[0]) + 1 +  " Solves") );
-
-            answer_input.val("");
+        answer_input.removeClass("correct");
+        answer_input.addClass("wrong");
+        setTimeout(function () {
             answer_input.removeClass("wrong");
-            answer_input.addClass("correct");
-        }
-        else if (result.status == 2){ // Challenge already solved
-            result_notification.addClass('alert alert-info alert-dismissable text-center');
-            result_notification.slideDown();
-
-            answer_input.addClass("correct");
-        }
-        else if (result.status == 3){ // Keys per minute too high
-            result_notification.addClass('alert alert-warning alert-dismissable text-center');
-            result_notification.slideDown();
-
-            answer_input.addClass("too-fast");
-            setTimeout(function() {
-                answer_input.removeClass("too-fast");
-            }, 3000);
-        }
-        marksolves();
-        updatesolves();
-        setTimeout(function(){
-          $('.alert').slideUp();
-          $('#submit-key').removeClass("disabled-button");
-          $('#submit-key').prop('disabled', false);
         }, 3000);
+    }
+    else if (result.status == 1) { // Challenge Solved
+        result_notification.addClass('alert alert-success alert-dismissable text-center');
+        result_notification.slideDown();
 
-        if (cb) {
-            cb(result);
-        }
-    })
+        $('.chal-solves').text((parseInt($('.chal-solves').text().split(" ")[0]) + 1 + " Solves"));
+
+        answer_input.val("");
+        answer_input.removeClass("wrong");
+        answer_input.addClass("correct");
+    }
+    else if (result.status == 2) { // Challenge already solved
+        result_notification.addClass('alert alert-info alert-dismissable text-center');
+        result_notification.slideDown();
+
+        answer_input.addClass("correct");
+    }
+    else if (result.status == 3) { // Keys per minute too high
+        result_notification.addClass('alert alert-warning alert-dismissable text-center');
+        result_notification.slideDown();
+
+        answer_input.addClass("too-fast");
+        setTimeout(function () {
+            answer_input.removeClass("too-fast");
+        }, 3000);
+    }
+    marksolves();
+    updatesolves();
+    setTimeout(function () {
+        $('.alert').slideUp();
+        $('#submit-key').removeClass("disabled-button");
+        $('#submit-key').prop('disabled', false);
+    }, 3000);
+
+    if (cb) {
+        cb(result);
+    }
 }
 
 function marksolves(cb) {
