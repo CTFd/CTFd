68c381053f9c7c2c662b5e5d50495a62fd8ae47d
Kevin Chung <kchung@nyu.edu>
2018-05-03 13:58:08 -0400

    Compare base64 encoded data with bytestring

--- tests/test_utils_encoding.py
+++ tests/test_utils_encoding.py
@@ -14,10 +14,10 @@ def test_urlsafe_base64_encode():
         assert urlsafe_base64_encode('user+user@ctfd.io') == 'dXNlcit1c2VyQGN0ZmQuaW8'
         assert urlsafe_base64_encode('😆') == '8J-Yhg'
     else:
-        assert urlsafe_base64_encode('abc123') == 'YWJjMTIz'
-        assert urlsafe_base64_encode('"test@mailinator.com".DGxeoA.lCssU3M2QuBfohO-FtdgDQLKbU4') == 'InRlc3RAbWFpbGluYXRvci5jb20iLkRHeGVvQS5sQ3NzVTNNMlF1QmZvaE8tRnRkZ0RRTEtiVTQ'
-        assert urlsafe_base64_encode('user+user@ctfd.io') == 'dXNlcit1c2VyQGN0ZmQuaW8'
-        assert urlsafe_base64_encode('😆') == '8J-Yhg'
+        assert urlsafe_base64_encode('abc123') == b'YWJjMTIz'
+        assert urlsafe_base64_encode('"test@mailinator.com".DGxeoA.lCssU3M2QuBfohO-FtdgDQLKbU4') == b'InRlc3RAbWFpbGluYXRvci5jb20iLkRHeGVvQS5sQ3NzVTNNMlF1QmZvaE8tRnRkZ0RRTEtiVTQ'
+        assert urlsafe_base64_encode('user+user@ctfd.io') == b'dXNlcit1c2VyQGN0ZmQuaW8'
+        assert urlsafe_base64_encode('😆') == b'8J-Yhg'
 
 
 def test_urlsafe_base64_decode():
@@ -29,10 +29,9 @@ def test_urlsafe_base64_decode():
         assert urlsafe_base64_decode('8J-Yhg==') == '😆'
         assert urlsafe_base64_decode('8J-Yhg') == '😆'
     else:
-        assert urlsafe_base64_decode('YWJjMTIz') == 'abc123'
-        assert urlsafe_base64_decode('YWJjMTIz') == 'abc123'
-        assert urlsafe_base64_decode('InRlc3RAbWFpbGluYXRvci5jb20iLkRHeGVvQS5sQ3NzVTNNMlF1QmZvaE8tRnRkZ0RRTEtiVTQ') == '"test@mailinator.com".DGxeoA.lCssU3M2QuBfohO-FtdgDQLKbU4'
-        assert urlsafe_base64_decode('dXNlcit1c2VyQGN0ZmQuaW8=') == 'user+user@ctfd.io'
-        assert urlsafe_base64_decode('dXNlcit1c2VyQGN0ZmQuaW8') == 'user+user@ctfd.io'
-        assert urlsafe_base64_decode('8J-Yhg==') == '😆'
-        assert urlsafe_base64_decode('8J-Yhg') == '😆'
+        assert urlsafe_base64_decode('YWJjMTIz') == b'abc123'
+        assert urlsafe_base64_decode('InRlc3RAbWFpbGluYXRvci5jb20iLkRHeGVvQS5sQ3NzVTNNMlF1QmZvaE8tRnRkZ0RRTEtiVTQ') == b'"test@mailinator.com".DGxeoA.lCssU3M2QuBfohO-FtdgDQLKbU4'
+        assert urlsafe_base64_decode('dXNlcit1c2VyQGN0ZmQuaW8=') == b'user+user@ctfd.io'
+        assert urlsafe_base64_decode('dXNlcit1c2VyQGN0ZmQuaW8') == b'user+user@ctfd.io'
+        assert urlsafe_base64_decode('8J-Yhg==') == b'😆'
+        assert urlsafe_base64_decode('8J-Yhg') == b'😆'
