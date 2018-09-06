506e19416f88101d7cf2c37e33e5db3d5d90becd
Kevin Chung <kchung@nyu.edu>
2018-09-05 21:52:34 -0400

    Basic modes wrappers

---
+++ CTFd/utils/modes/__init__.py
@@ -0,0 +1,14 @@
+from CTFd.utils import get_config, set_config
+
+TEAM_SHARED = "user_team_login"
+USER = "user_login"
+TEAM_INDIVIDUAL = "team_login"
+
+def get_user_mode():
+    pass
+
+def set_user_mode():
+    pass
+
+def allowed_in_current_user_mode(ctx):
+    pass
