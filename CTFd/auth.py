63ff2b445b9ea20a60e8d4b415697660797c2811
Kevin Chung <kchung@nyu.edu>
2018-10-24 00:30:07 -0400

    Write a logging wrapper

--- CTFd/auth.py
+++ CTFd/auth.py
@@ -11,11 +11,10 @@ from CTFd.utils import user as current_user
 from CTFd.utils import config, validators
 from CTFd.utils import email
 from CTFd.utils.security.auth import login_user, logout_user
+from CTFd.utils.logging import log
 
 import base64
-import logging
 import requests
-import time
 
 auth = Blueprint('auth', __name__)
 
@@ -28,7 +27,6 @@ def confirm(data=None):
         # If the CTF doesn't care about confirming email addresses then redierct to challenges
         return redirect(url_for('challenges.challenges_view'))
 
-    logger = logging.getLogger('regs')
     # User is confirming email account
     if data and request.method == "GET":
         try:
@@ -40,13 +38,8 @@ def confirm(data=None):
             return render_template('confirm.html', errors=['Your confirmation token is invalid'])
         team = Users.query.filter_by(email=user_email).first_or_404()
         team.verified = True
+        log('registrations', format="[{date}] {ip} -  successful password reset for {username}")
         db.session.commit()
-        logger.warn("[{date}] {ip} - {username} confirmed their account".format(
-            date=time.strftime("%m/%d/%Y %X"),
-            ip=current_user.get_ip(),
-            username=team.name.encode('utf-8'),
-            email=team.email.encode('utf-8')
-        ))
         db.session.close()
         if current_user.authed():
             return redirect(url_for('challenges.challenges_view'))
@@ -65,12 +58,7 @@ def confirm(data=None):
                 return redirect(url_for('views.profile'))
             else:
                 email.verify_email_address(team.email)
-                logger.warn("[{date}] {ip} - {username} initiated a confirmation email resend".format(
-                    date=time.strftime("%m/%d/%Y %X"),
-                    ip=current_user.get_ip(),
-                    username=team.name.encode('utf-8'),
-                    email=team.email.encode('utf-8')
-                ))
+                log('registrations', format="[{date}] {ip} - {username} initiated a confirmation email resend")
             return render_template('confirm.html', team=team, infos=['Your confirmation email has been resent!'])
         elif request.method == "GET":
             # User has been directed to the confirm page
@@ -86,8 +74,6 @@ def confirm(data=None):
 @auth.route('/reset_password/<data>', methods=['POST', 'GET'])
 @ratelimit(method="POST", limit=10, interval=60)
 def reset_password(data=None):
-    logger = logging.getLogger('logins')
-
     if data is not None:
         try:
             s = TimedSerializer(app.config['SECRET_KEY'])
@@ -103,11 +89,7 @@ def reset_password(data=None):
             team = Users.query.filter_by(name=name).first_or_404()
             team.password = bcrypt_sha256.encrypt(request.form['password'].strip())
             db.session.commit()
-            logger.warn("[{date}] {ip} -  successful password reset for {username}".format(
-                date=time.strftime("%m/%d/%Y %X"),
-                ip=current_user.get_ip(),
-                username=team.name.encode('utf-8')
-            ))
+            log('logins', format="[{date}] {ip} -  successful password reset for {username}")
             db.session.close()
             return redirect(url_for('auth.login'))
 
@@ -141,7 +123,6 @@ def reset_password(data=None):
 @auth.route('/register', methods=['POST', 'GET'])
 @ratelimit(method="POST", limit=10, interval=5)
 def register():
-    logger = logging.getLogger('regs')
     if not config.can_register():
         return redirect(url_for('auth.login'))
     if request.method == 'POST':
@@ -206,13 +187,7 @@ def register():
                 login_user(user)
 
                 if config.can_send_mail() and get_config('verify_emails'):  # Confirming users is enabled and we can send email.
-                    logger = logging.getLogger('regs')
-                    logger.warn("[{date}] {ip} - {username} registered (UNCONFIRMED) with {email}".format(
-                        date=time.strftime("%m/%d/%Y %X"),
-                        ip=current_user.get_ip(),
-                        username=request.form['name'].encode('utf-8'),
-                        email=request.form['email'].encode('utf-8')
-                    ))
+                    log('registrations', format="[{date}] {ip} - {name} registered (UNCONFIRMED) with {email}")
                     email_address.verify_email_address(user.email)
                     db.session.close()
                     return redirect(url_for('auth.confirm'))
@@ -223,12 +198,7 @@ def register():
                             "You've successfully registered for {}".format(get_config('ctf_name'))
                         )
 
-        logger.warn("[{date}] {ip} - {username} registered with {email}".format(
-            date=time.strftime("%m/%d/%Y %X"),
-            ip=current_user.get_ip(),
-            username=request.form['name'].encode('utf-8'),
-            email=request.form['email'].encode('utf-8')
-        ))
+        log('registrations', "[{date}] {ip} - {name} registered with {email}")
         db.session.close()
         return redirect(url_for('challenges.challenges_view'))
     else:
@@ -238,7 +208,6 @@ def register():
 @auth.route('/login', methods=['POST', 'GET'])
 @ratelimit(method="POST", limit=10, interval=5)
 def login():
-    logger = logging.getLogger('logins')
     if request.method == 'POST':
         errors = []
         name = request.form['name']
@@ -257,38 +226,25 @@ def login():
                     pass  # TODO: Some session objects don't implement regenerate :(
 
                 login_user(user)
+                log('logins', "[{date}] {ip} - {name} logged in")
 
                 db.session.close()
-
-                logger.warn("[{date}] {ip} - {username} logged in".format(
-                    date=time.strftime("%m/%d/%Y %X"),
-                    ip=current_user.get_ip(),
-                    username=session['name'].encode('utf-8')
-                ))
-
                 if request.args.get('next') and validators.is_safe_url(request.args.get('next')):
                     return redirect(request.args.get('next'))
                 return redirect(url_for('challenges.challenges_view'))
 
-            else:  # This user exists but the password is wrong
-                logger.warn("[{date}] {ip} - submitted invalid password for {username}".format(
-                    date=time.strftime("%m/%d/%Y %X"),
-                    ip=current_user.get_ip(),
-                    username=user.name.encode('utf-8')
-                ))
+            else:
+                # This user exists but the password is wrong
+                log('logins', "[{date}] {ip} - submitted invalid password for {name}")
                 errors.append("Your username or password is incorrect")
                 db.session.close()
                 return render_template('login.html', errors=errors)
-
-        else:  # This user just doesn't exist
-            logger.warn("[{date}] {ip} - submitted invalid account information".format(
-                date=time.strftime("%m/%d/%Y %X"),
-                ip=current_user.get_ip()
-            ))
+        else:
+            # This user just doesn't exist
+            log('logins', "[{date}] {ip} - submitted invalid account information")
             errors.append("Your username or password is incorrect")
             db.session.close()
             return render_template('login.html', errors=errors)
-
     else:
         db.session.close()
         return render_template('login.html')
