c7d0d244534af0f9efc5ea887ad87265fb078aef
Kevin Chung <kchung@nyu.edu>
2018-10-25 03:24:11 -0400

    Update the update_check function to send up more anonymous data for statistics purposes.

--- CTFd/utils/updates/__init__.py
+++ CTFd/utils/updates/__init__.py
@@ -1,11 +1,22 @@
 from distutils.version import StrictVersion
 from flask import current_app as app
-from CTFd.utils import get_config, set_config
+from CTFd.utils import get_config, set_config, get_app_config
+from CTFd.models import db, Challenges, Users, Teams
+from platform import python_version
 import requests
 import time
+import sys
 
 
 def update_check(force=False):
+    """
+    Makes a request to ctfd.io to check if there is a new version of CTFd available. The service is provided in return
+    for users opting in to anonymous usage data collection. Users can opt-out of update checks by specifying
+    UPDATE_CHECK = False in config.py
+
+    :param force:
+    :return:
+    """
     # If UPDATE_CHECK is disabled don't check for updates at all.
     if app.config.get('UPDATE_CHECK') is False:
         return
@@ -19,11 +30,20 @@ def update_check(force=False):
     if update:
         try:
             params = {
-                'current': app.VERSION
+                'current': app.VERSION,
+                'python_version_raw': sys.hexversion,
+                'python_version': python_version(),
+                'db_driver': db.session.bind.dialect.name,
+                'challenge_count': Challenges.query.count(),
+                'user_mode': get_config('user_mode'),
+                'user_count': Users.query.count(),
+                'team_count': Teams.query.count(),
+                'theme': get_config('ctf_theme'),
+                'upload_provider': get_app_config('UPLOAD_PROVIDER')
             }
-            check = requests.get(
-                'https://versioning.ctfd.io/versions/latest',
-                params=params,
+            check = requests.post(
+                'https://versioning.ctfd.io/',
+                json=params,
                 timeout=0.1
             ).json()
         except requests.exceptions.RequestException as e:
@@ -40,6 +60,7 @@ def update_check(force=False):
                 set_config('version_latest', None)
         finally:
             # 12 hours later
+            # TODO: This should be set by the server response
             next_update_check_time = int(time.time() + 43200)
             set_config('next_update_check', next_update_check_time)
     else:
