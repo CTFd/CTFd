2dcec865038d6255307d1290f5033dc5761d281a
Kevin Chung <kchung@nyu.edu>
2018-10-28 05:21:19 -0400

    Fix events to work under subdirectories

--- CTFd/themes/core/static/js/events.js
+++ CTFd/themes/core/static/js/events.js
@@ -1,4 +1,9 @@
-var socket = io.connect(window.location.protocol + '//' + document.domain + ':' + location.port);
+var socket = io.connect(
+    window.location.protocol + '//' + document.domain + ':' + location.port,
+    {
+        path: script_root + '/socket.io'
+    }
+);
 
 socket.on('notification', function (data) {
     ezal({
