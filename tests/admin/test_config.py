01c9d32081b14d64a2d7c38239a66dce2bdf9d43
Kevin Chung <kchung@nyu.edu>
2018-11-09 13:02:30 -0500

    Add a test for reset CTF functionality

--- tests/admin/test_config.py
+++ tests/admin/test_config.py
@@ -1,3 +1,4 @@
+from CTFd.models import Users, Challenges
 from tests.helpers import *
 
 
@@ -8,4 +9,34 @@ def test_get_admin_config():
         client = login_as_user(app, name="admin", password="password")
         r = client.get('/admin/config')
         assert r.status_code == 200
+    destroy_ctfd(app)
+
+
+def test_reset():
+    app = create_ctfd()
+    with app.app_context():
+        base_user = 'user'
+        for x in range(10):
+            user = base_user + str(x)
+            user_email = user + "@ctfd.io"
+            gen_user(app.db, name=user, email=user_email)
+
+        for x in range(10):
+            chal = gen_challenge(app.db, name='chal_name{}'.format(x))
+            gen_flag(app.db, challenge_id=chal.id, content='flag')
+
+        assert Users.query.count() == 11  # 11 because of the first admin user
+        assert Challenges.query.count() == 10
+
+        register_user(app)
+        client = login_as_user(app, name="admin", password="password")
+
+        with client.session_transaction() as sess:
+            data = {
+                "nonce": sess.get('nonce')
+            }
+            r = client.post('/admin/reset', data=data)
+
+        assert Users.query.count() == 0
+        assert Challenges.query.count() == 10
     destroy_ctfd(app)
