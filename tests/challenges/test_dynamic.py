56cf299fad714af605eab96fb439fe14ae3bae76
Peter <fejerpeter93@gmail.com>
2018-12-06 09:05:28 +0100

    Update test_dynamic.py
    Creating unit test for adding requirement to dynamic challenges

--- tests/challenges/test_dynamic.py
+++ tests/challenges/test_dynamic.py
@@ -83,6 +83,62 @@ def test_can_update_dynamic_challenge():
         assert challenge.state == "visible"
 
     destroy_ctfd(app)
+    
+def test_can_add_requirement_dynamic_challenge():
+    """Test that dynamic challenges can be deleted"""
+    app = create_ctfd(enable_plugins=True)
+    with app.app_context():
+        challenge_data = {
+            "name": "name",
+            "category": "category",
+            "description": "description",
+            "value": 100,
+            "decay": 20,
+            "minimum": 1,
+            "state": "hidden",
+            "type": "dynamic"
+        }
+        req = FakeRequest(form=challenge_data)
+        challenge = DynamicValueChallenge.create(req)
+
+        assert challenge.value == 100
+        assert challenge.initial == 100
+        assert challenge.decay == 20
+        assert challenge.minimum == 1
+
+        challenge_data = {
+            "name": "second_name",
+            "category": "category",
+            "description": "new_description",
+            "value": "200",
+            "initial": "200",
+            "decay": "40",
+            "minimum": "5",
+            "max_attempts": "0",
+            "state": "visible"
+        }
+
+        req = FakeRequest(form=challenge_data)
+        challenge = DynamicValueChallenge.create(challenge, req)
+
+        assert challenge.name == 'second_name'
+        assert challenge.description == "new_description"
+        assert challenge.value == 200
+        assert challenge.initial == 200
+        assert challenge.decay == 40
+        assert challenge.minimum == 5
+        assert challenge.state == "visible"
+
+        challenge_data = {
+            "requirements": [1]
+        }
+
+        req = FakeRequest(form=challenge_data)
+        challenge = DynamicValueChallenge.update(challenge, req)
+
+        assert challenge.requirements == [1]
+
+    destroy_ctfd(app)
 
 
 def test_can_delete_dynamic_challenge():
