ae8ce0b4309fa62d5781b26754c6f7463c32e584
Kevin Chung <kchung@nyu.edu>
2019-01-24 02:47:50 -0500

    Install gevent-websocket and use it by default until we have a better solution (#850)
    * Install `gevent-websocket` and use it by default until we have a better solution
    * May help with #849

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,6 +2,7 @@
 set -eo pipefail
 
 WORKERS=${WORKERS:-1}
+WORKER_CLASS=${WORKER_CLASS:-geventwebsocket.gunicorn.workers.GeventWebSocketWorker}
 ACCESS_LOG=${ACCESS_LOG:--}
 ERROR_LOG=${ERROR_LOG:--}
 
@@ -40,6 +41,6 @@ echo "Starting CTFd"
 exec gunicorn 'CTFd:create_app()' \
     --bind '0.0.0.0:8000' \
     --workers $WORKERS \
-    --worker-class 'gevent' \
+    --worker-class "$WORKER_CLASS" \
     --access-logfile "$ACCESS_LOG" \
     --error-logfile "$ERROR_LOG"
