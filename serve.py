3b7f5ebe50ab13a71ea8f56e4886292918d0c675
Kevin Chung <kchung@nyu.edu>
2019-03-17 02:56:27 -0400

    Add Flask-Profiler to development requirements. Add a --profile option to serve.py

--- serve.py
+++ serve.py
@@ -1,4 +1,24 @@
 from CTFd import create_app
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--profile", help="Enable flask_profiler profiling", action="store_true")
+args = parser.parse_args()
 
 app = create_app()
+
+if args.profile:
+    import flask_profiler
+    app.config["flask_profiler"] = {
+        "enabled": app.config["DEBUG"],
+        "storage": {
+            "engine": "sqlite"
+        },
+        "basicAuth": {
+            "enabled": False,
+        },
+    }
+    flask_profiler.init_app(app)
+    print(" * Flask profiling running at http://127.0.0.1:4000/flask-profiler/")
+
 app.run(debug=True, threaded=True, host="127.0.0.1", port=4000)
