77a96c51b6b15eb123da845280aa72709cba3ea2
Kevin Chung <kchung@nyu.edu>
2019-04-04 01:38:33 -0400

    Use localStorage to bypass TCP connection limits in browsers for SSE

--- CTFd/themes/core/static/js/events.js
+++ CTFd/themes/core/static/js/events.js
@@ -1,10 +1,38 @@
-var source = new EventSource(script_root + "/events");
+var wc = new WindowController();
 
-source.addEventListener('notification', function (event) {
-    var data = JSON.parse(event.data);
+function connect() {
+    window.ctfEventSource = new EventSource(script_root + "/events");
+
+    window.ctfEventSource.addEventListener('notification', function (event) {
+        var data = JSON.parse(event.data);
+        wc.broadcast('notification', data);
+        render(data);
+    }, false);
+}
+
+function disconnect() {
+    if (window.ctfEventSource) {
+        window.ctfEventSource.close();
+    }
+}
+
+function render(data) {
     ezal({
         title: data.title,
         body: data.content,
         button: "Got it!"
     });
-}, false);
+}
+
+wc.notification = function(data) {
+    render(data);
+};
+
+wc.masterDidChange = function () {
+    console.log(this.isMaster);
+    if (this.isMaster) {
+        connect();
+    } else {
+        disconnect();
+    }
+};
