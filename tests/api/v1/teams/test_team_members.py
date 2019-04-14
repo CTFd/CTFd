7a378f45b37046963414c45bcf2522134178018d
Kevin Chung <kchung@nyu.edu>
2019-04-14 19:38:22 -0400

    Fix test

--- tests/api/v1/teams/test_team_members.py
+++ tests/api/v1/teams/test_team_members.py
@@ -83,29 +83,30 @@ def test_api_admin_can_change_captain():
         user2.team_id = team.id
         app.db.session.commit()
 
-    # I am not the captain
-    with login_as_user(app, name="user2") as client:
-        r = client.patch('/api/v1/teams/1', json={
-            'captain_id': 3
-        })
-        assert r.status_code == 403
+        # I am not the captain
+        with login_as_user(app, name="user2") as client:
+            r = client.patch('/api/v1/teams/1', json={
+                'captain_id': 3
+            })
+            assert r.status_code == 403
 
-    # Look at me, I'm the captain now
-    with login_as_user(app, name="user1") as client:
-        r = client.patch('/api/v1/teams/1', json={
-            'captain_id': 3
-        })
-        # We should still receive a 403 because admins are the only people who can change captains for specific teams
-        assert r.status_code == 403
+        # Look at me, I'm the captain now
+        with login_as_user(app, name="user1") as client:
+            r = client.patch('/api/v1/teams/1', json={
+                'captain_id': 3
+            })
+            # We should still receive a 403 because admins are the only people who can change captains for specific teams
+            assert r.status_code == 403
 
-    # Escalate to admin
-    with login_as_user(app, name="admin") as client:
-        r = client.patch('/api/v1/teams/1', json={
-            'captain_id': 3
-        })
-        resp = r.get_json()
-        assert resp['data']['captain_id'] == 3
-        assert r.status_code == 200
+        # Escalate to admin
+        with login_as_user(app, name="admin") as client:
+            r = client.patch('/api/v1/teams/1', json={
+                'captain_id': 3
+            })
+            resp = r.get_json()
+            assert resp['data']['captain_id'] == 3
+            assert r.status_code == 200
+    destroy_ctfd(app)
 
 
 def test_api_users_can_change_captain_on_self_team():
@@ -122,18 +123,19 @@ def test_api_users_can_change_captain_on_self_team():
         user2.team_id = team.id
         app.db.session.commit()
 
-    # I am not the captain
-    with login_as_user(app, name="user2") as client:
-        r = client.patch('/api/v1/teams/me', json={
-            'captain_id': 3
-        })
-        assert r.status_code == 400
+        # I am not the captain
+        with login_as_user(app, name="user2") as client:
+            r = client.patch('/api/v1/teams/me', json={
+                'captain_id': 3
+            })
+            assert r.status_code == 400
 
-    # Look at me, I'm the captain now
-    with login_as_user(app, name="user1") as client:
-        r = client.patch('/api/v1/teams/me', json={
-            'captain_id': 3
-        })
-        resp = r.get_json()
-        assert resp['data']['captain_id'] == 3
-        assert r.status_code == 200
+        # Look at me, I'm the captain now
+        with login_as_user(app, name="user1") as client:
+            r = client.patch('/api/v1/teams/me', json={
+                'captain_id': 3
+            })
+            resp = r.get_json()
+            assert resp['data']['captain_id'] == 3
+            assert r.status_code == 200
+    destroy_ctfd(app)
