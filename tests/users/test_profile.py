5264a910ada9c8064082a1a68370d2ec38d57a04
Kevin Chung <kchung@nyu.edu>
2019-08-04 18:32:05 -0400

    Require password for email changes

---
+++ tests/users/test_profile.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from CTFd.models import db, Users
+from CTFd.utils import set_config, get_config
+from CTFd.utils.security.signing import serialize
+from CTFd.utils.crypto import verify_password
+from freezegun import freeze_time
+from tests.helpers import create_ctfd, destroy_ctfd, register_user, login_as_user
+from mock import patch
+
+
+def test_email_cannot_be_changed_without_password():
+    """Test that a user can't update their email address without current password"""
+    app = create_ctfd()
+    with app.app_context():
+        register_user(app)
+        client = login_as_user(app)
+
+        data = {
+            "name": "user",
+            "email": "user2@ctfd.io",
+        }
+
+        r = client.patch("/api/v1/users/me", json=data)
+        assert r.status_code == 400
+        user = Users.query.filter_by(id=2).first()
+        assert user.email == "user@ctfd.io"
+
+        data = {
+            "name": "user",
+            "email": "user2@ctfd.io",
+            "confirm": "asdf",
+        }
+
+        r = client.patch("/api/v1/users/me", json=data)
+        assert r.status_code == 400
+        user = Users.query.filter_by(id=2).first()
+        assert user.email == "user@ctfd.io"
+
+        data = {
+            "name": "user",
+            "email": "user2@ctfd.io",
+            "confirm": "password",
+        }
+
+        r = client.patch("/api/v1/users/me", json=data)
+        assert r.status_code == 200
+        user = Users.query.filter_by(id=2).first()
+        assert user.email == "user2@ctfd.io"
+    destroy_ctfd(app)
