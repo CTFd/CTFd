c088abaa32566a101c4441d995b0ede323b86a06
Kevin Chung <kchung@nyu.edu>
2019-09-05 19:14:48 -0400

    Fix flask run by not monkey patching for gevent in wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,14 @@
-from gevent import monkey
-monkey.patch_all()
+import os
+
+# Detect if we're running via `flask run` and don't monkey patch
+if not os.getenv("FLASK_RUN_FROM_CLI"):
+    from gevent import monkey
+
+    monkey.patch_all()
+
 from CTFd import create_app
 
 app = create_app()
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True, threaded=True, host="127.0.0.1", port=4000)
