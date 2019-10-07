9a46bf09168ed14fb203969006fa7b4feed46f10
Kevin Chung <kchung@ctfd.io>
2019-10-07 00:31:09 -0700

    Seperate dev and prod static assets (#1128)
    * Use a custom `url_for` function in themes
    * Modify webpack and yarn to generate development and production js
    * Fix `test_update_check_ignores_downgrades`

--- CTFd/utils/helpers/__init__.py
+++ CTFd/utils/helpers/__init__.py
@@ -1,4 +1,11 @@
-from flask import request, flash, get_flashed_messages
+from flask import (
+    request,
+    flash,
+    get_flashed_messages,
+    url_for as flask_url_for,
+    current_app
+)
+import os
 
 
 def info_for(endpoint, message):
@@ -15,3 +22,16 @@ def get_infos():
 
 def get_errors():
     return get_flashed_messages(category_filter=request.endpoint + ".errors")
+
+
+def url_for(endpoint, **values):
+    if endpoint == 'views.themes':
+        path = values.get('path', '')
+        static_asset = path.endswith('.js') or path.endswith('.css')
+        direct_access = '.dev' in path or '.min' in path
+        if static_asset and not direct_access:
+            env = values.get('env', current_app.env)
+            mode = '.dev' if env == 'development' else '.min'
+            base, ext = os.path.splitext(path)
+            values['path'] = base + mode + ext
+    return flask_url_for(endpoint, **values)
