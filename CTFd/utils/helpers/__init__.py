11f108a71e7ed5b519a076e21c488798774c7a4f
Kevin Chung <kchung@nyu.edu>
2019-10-06 23:01:41 -0700

    Add custom url_for function to select development or production assets

--- CTFd/utils/helpers/__init__.py
+++ CTFd/utils/helpers/__init__.py
@@ -1,4 +1,11 @@
-from flask import request, flash, get_flashed_messages
+import os
+from flask import (
+    request,
+    flash,
+    get_flashed_messages,
+    url_for as flask_url_for,
+    current_app
+)
 
 
 def info_for(endpoint, message):
@@ -15,3 +22,14 @@ def get_infos():
 
 def get_errors():
     return get_flashed_messages(category_filter=request.endpoint + ".errors")
+
+
+def url_for(endpoint, **values):
+    if endpoint == 'views.themes':
+        path = values.get('path', '')
+        if '.dev' not in path and '.min' not in path:
+            env = values.get('env', current_app.env)
+            mode = '.dev' if env == 'development' else '.min'
+            base, ext = os.path.splitext(path)
+            values['path'] = base + mode + ext
+    return flask_url_for(endpoint, **values)
