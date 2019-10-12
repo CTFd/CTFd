d16ded59222361ca34e071d00e5c6b3bb5e58ee0
Kevin Chung <kchung@nyu.edu>
2019-10-12 00:54:54 -0700

    Clean up test

--- tests/utils/test_formatters.py
+++ tests/utils/test_formatters.py
@@ -5,6 +5,4 @@ def test_safe_format():
     assert format("Message from {ctf_name}", ctf_name="CTF") == "Message from CTF"
     assert format("Message from {{ ctf_name }}", ctf_name="CTF") == "Message from CTF"
     assert format("{{ ctf_name }} {{ctf_name}}", ctf_name="CTF") == "CTF CTF"
-    assert (
-        format("{{ ctf_name }} {{ctf_name}} {asdf}", ctf_name="CTF") == "CTF CTF {asdf}"
-    )
+    assert format("{ ctf_name } {ctf_name} {asdf}", ctf_name="CTF") == "CTF CTF {asdf}"
