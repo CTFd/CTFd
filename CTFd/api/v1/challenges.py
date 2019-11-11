d8c188a20fb4557e661fa1d363893a3f8bb6c110
Justin Chadwell <jedevc@gmail.com>
2019-11-11 11:18:32 +0000

    Add show argument to /api/v1/challenges
    The show argument can be set to "all" to display all challenges,
    including hidden and locked challenges and ones for which their
    dependencies have not been completed. This functionality is only
    available to admins.

--- CTFd/api/v1/challenges.py
+++ CTFd/api/v1/challenges.py
@@ -58,31 +58,38 @@ class ChallengeList(Resource):
         # This can return None (unauth) if visibility is set to public
         user = get_current_user()
 
-        challenges = (
-            Challenges.query.filter(
-                and_(Challenges.state != "hidden", Challenges.state != "locked")
-            )
-            .order_by(Challenges.value)
-            .all()
-        )
-
-        if user:
-            solve_ids = (
-                Solves.query.with_entities(Solves.challenge_id)
-                .filter_by(account_id=user.account_id)
-                .order_by(Solves.challenge_id.asc())
+        if is_admin() and request.args.get("show") == "all":
+            challenges = (
+                Challenges.query
+                .order_by(Challenges.value)
                 .all()
             )
-            solve_ids = set([value for value, in solve_ids])
-
-            # TODO: Convert this into a re-useable decorator
-            if is_admin():
-                pass
-            else:
-                if config.is_teams_mode() and get_current_team() is None:
-                    abort(403)
+            solve_ids = set([challenge.id for challenge in challenges])
         else:
-            solve_ids = set()
+            challenges = (
+                Challenges.query.filter(
+                    and_(Challenges.state != "hidden", Challenges.state != "locked")
+                )
+                .order_by(Challenges.value)
+                .all()
+            )
+            if user:
+                solve_ids = (
+                    Solves.query.with_entities(Solves.challenge_id)
+                    .filter_by(account_id=user.account_id)
+                    .order_by(Solves.challenge_id.asc())
+                    .all()
+                )
+                solve_ids = set([value for value, in solve_ids])
+
+                # TODO: Convert this into a re-useable decorator
+                if is_admin():
+                    pass
+                else:
+                    if config.is_teams_mode() and get_current_team() is None:
+                        abort(403)
+            else:
+                solve_ids = set()
 
         response = []
         tag_schema = TagSchema(view="user", many=True)
