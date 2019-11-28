2bd0bd6300b089704f4c95a5910a836dcad0da47
Kevin Chung <kchung@nyu.edu>
2019-11-28 02:30:39 -0500

    Handle input validation between tabs

--- CTFd/themes/core/assets/js/pages/setup.js
+++ CTFd/themes/core/assets/js/pages/setup.js
@@ -4,6 +4,26 @@ import CTFd from "../CTFd";
 
 function switchTab(event) {
   event.preventDefault();
+
+  // Handle tab validation
+  let valid_tab = true;
+  $(event.target)
+    .closest("[role=tabpanel]")
+    .find("input,textarea")
+    .each(function(i, e) {
+      $e = $(e);
+      let status = e.checkValidity();
+      if (status === false) {
+        $e.removeClass("input-filled-valid");
+        $e.addClass("input-filled-invalid");
+        valid_tab = false;
+      }
+    });
+
+  if (valid_tab == false) {
+    return;
+  }
+
   let href = $(event.target).data("href");
   $(`.nav a[href="${href}"]`).tab("show");
 }
