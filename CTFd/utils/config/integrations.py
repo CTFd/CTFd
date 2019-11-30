180b05cb866fe5615990829ecfcda071aa873416
Kevin Chung <kchung@nyu.edu>
2019-11-30 04:24:11 -0500

    Run black

--- CTFd/utils/config/integrations.py
+++ CTFd/utils/config/integrations.py
@@ -1,5 +1,5 @@
-from flask import current_app as app
 from CTFd.utils import get_config
 
+
 def mlc():
-    return get_config('oauth_client_id') and get_config('oauth_client_secret')
+    return get_config("oauth_client_id") and get_config("oauth_client_secret")
