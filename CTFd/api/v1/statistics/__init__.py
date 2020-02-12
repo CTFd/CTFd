69544cbdba1b9aa66832dd8c824f1d0331e7acd0
Kevin Chung <kchung@nyu.edu>
2020-02-12 00:16:12 -0500

    Fix another import

--- CTFd/api/v1/statistics/__init__.py
+++ CTFd/api/v1/statistics/__init__.py
@@ -1,10 +1,10 @@
 from flask_restplus import Namespace
 
+statistics_namespace = Namespace(
+    "statistics", description="Endpoint to retrieve Statistics"
+)
+
 from CTFd.api.v1.statistics import challenges  # noqa: F401
 from CTFd.api.v1.statistics import submissions  # noqa: F401
 from CTFd.api.v1.statistics import teams  # noqa: F401
 from CTFd.api.v1.statistics import users  # noqa: F401
-
-statistics_namespace = Namespace(
-    "statistics", description="Endpoint to retrieve Statistics"
-)
