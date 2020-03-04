4608f54df63bad22b80b09ef266cf48e05281c86
Kevin Chung <kchung@nyu.edu>
2020-03-04 00:16:58 -0500

    Set UserStatistics route to be admins_only

--- CTFd/api/v1/statistics/users.py
+++ CTFd/api/v1/statistics/users.py
@@ -8,6 +8,7 @@ from CTFd.utils.decorators import admins_only
 
 @statistics_namespace.route("/users")
 class UserStatistics(Resource):
+    @admins_only
     def get(self):
         registered = Users.query.count()
         confirmed = Users.query.filter_by(verified=True).count()
