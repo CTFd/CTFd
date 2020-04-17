c71c5442f8e9069da5e9af0807b564d5880b0ac2
Kevin Chung <kchung@nyu.edu>
2020-04-16 20:27:07 -0400

    Finish and add graph to admin panel

--- CTFd/api/v1/statistics/scores.py
+++ CTFd/api/v1/statistics/scores.py
@@ -1,8 +1,9 @@
+from collections import defaultdict
+
 from flask_restx import Resource
-from sqlalchemy import func
 
 from CTFd.api.v1.statistics import statistics_namespace
-from CTFd.models import Submissions
+from CTFd.models import db, Challenges
 from CTFd.utils.decorators import admins_only
 from CTFd.utils.scores import get_standings
 
@@ -18,11 +19,22 @@ class ScoresDistribution(Resource):
             .first()
             .sum
         )
-        # Divide score by challenges to get brackets
+        # Divide score by challenges to get brackets with explicit floor division
         bracket_size = total_points // challenge_count
 
         # Get standings
         standings = get_standings(admin=True)
 
         # Iterate over standings and increment the count for each bracket for each standing within that bracket
-        return {"success": True, "data": data}
+        bottom, top = 0, bracket_size
+        count = 1
+        brackets = defaultdict(lambda: 0)
+        for t in reversed(standings):
+            if ((t.score >= bottom) and (t.score <= top)) or t.score <= 0:
+                brackets[top] += 1
+            else:
+                count += 1
+                bottom, top = (bracket_size, (bracket_size * count))
+                brackets[top] += 1
+
+        return {"success": True, "data": {"brackets": brackets}}
