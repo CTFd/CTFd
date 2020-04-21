7ddb71ad288c5e1d311dd7e3eb89b1b57fc8785a
Kevin Chung <kchung@nyu.edu>
2020-04-21 03:00:35 -0400

    Add manage.py

--- manage.py
+++ manage.py
@@ -12,6 +12,21 @@ manager = Manager(app)
 manager.add_command("db", MigrateCommand)
 
 
+def jsenums():
+    from CTFd.constants import JS_ENUMS
+    import json
+    import os
+
+    path = os.path.join(app.root_path, "themes/core/assets/js/constants.js")
+
+    with open(path, "w+") as f:
+        for k, v in JS_ENUMS.items():
+            f.write("const {} = Object.freeze({});".format(k, json.dumps(v)))
+
+
+BUILD_COMMANDS = {"jsenums": jsenums}
+
+
 @manager.command
 def get_config(key):
     with app.app_context():
@@ -22,6 +37,13 @@ def get_config(key):
 def set_config(key, value):
     with app.app_context():
         print(set_config_util(key, value).value)
+
+
+@manager.command
+def build(cmd):
+    with app.app_context():
+        cmd = BUILD_COMMANDS.get(cmd)
+        cmd()
 
 
 if __name__ == "__main__":
