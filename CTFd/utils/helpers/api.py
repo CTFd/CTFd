0c58e8e28b9b5a4f6431adaaf1b29a6e59106c2b
Kevin Chung <kchung@nyu.edu>
2020-04-21 02:48:26 -0400

    Add query and location

--- CTFd/utils/helpers/api.py
+++ CTFd/utils/helpers/api.py
@@ -1,11 +1,13 @@
 from CTFd.constants.api import OPENAPI_TYPE_MAPPING
 
 
-def build_endpoint_docs(args):
+def build_endpoint_docs(args, location="query"):
     docs = {}
     for k, v in args.items():
         docs[k] = {
             "description": v.metadata.get("doc", ""),
             "type": OPENAPI_TYPE_MAPPING.get(type(v).__name__),
+            "in": location,
+            "required": v.required
         }
     return docs
