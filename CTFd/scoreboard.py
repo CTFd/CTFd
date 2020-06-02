855c0bb58e23779b674c9ed9b57a4f080ef37def
Kevin Chung <kchung@nyu.edu>
2020-06-02 19:27:48 -0400

    Scoreboard using error component

--- CTFd/scoreboard.py
+++ CTFd/scoreboard.py
@@ -4,6 +4,7 @@ from CTFd.cache import cache, make_cache_key
 from CTFd.utils import config
 from CTFd.utils.decorators.visibility import check_score_visibility
 from CTFd.utils.scores import get_standings
+from CTFd.utils.helpers import get_errors, get_infos
 
 scoreboard = Blueprint("scoreboard", __name__)
 
@@ -12,9 +13,14 @@ scoreboard = Blueprint("scoreboard", __name__)
 @check_score_visibility
 @cache.cached(timeout=60, key_prefix=make_cache_key)
 def listing():
+    infos = get_infos()
+
+    if config.is_scoreboard_frozen():
+        infos.append("Scoreboard has been frozen")
+
     standings = get_standings()
     return render_template(
         "scoreboard.html",
         standings=standings,
-        score_frozen=config.is_scoreboard_frozen(),
+        infos=infos,
     )
