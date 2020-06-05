c65aa68ad552801228e311baf79406ae8a43ba5b
Kevin Chung <kchung@nyu.edu>
2020-06-04 21:09:33 -0400

    Add a few other form classes

--- CTFd/forms/auth.py
+++ CTFd/forms/auth.py
@@ -8,3 +8,23 @@ class RegistrationForm(BaseForm):
     email = StringField("Email")
     password = PasswordField("Password")
     submit = SubmitField("Submit")
+
+
+class LoginForm(BaseForm):
+    name = StringField("User Name or Email")
+    password = PasswordField("Password")
+    submit = SubmitField("Submit")
+
+
+class ConfirmForm(BaseForm):
+    submit = SubmitField("Resend")
+
+
+class ResetPasswordRequestForm(BaseForm):
+    email = StringField("Email")
+    submit = SubmitField("Submit")
+
+
+class ResetPasswordForm(BaseForm):
+    password = PasswordField("Password")
+    submit = SubmitField("Submit")
