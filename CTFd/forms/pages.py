6ffb9887e1859ad6f4aa8bb63f9e97701b4e5e01
Kevin Chung <kchung@nyu.edu>
2020-06-07 22:06:58 -0400

    Update page editting form to wtforms

--- CTFd/forms/pages.py
+++ CTFd/forms/pages.py
@@ -1,7 +1,27 @@
-from wtforms import HiddenField, MultipleFileField
+from wtforms import (
+    BooleanField,
+    HiddenField,
+    MultipleFileField,
+    StringField,
+    TextAreaField,
+)
 from wtforms.validators import InputRequired
 
 from CTFd.forms import BaseForm
+
+
+class PageEditForm(BaseForm):
+    title = StringField(
+        "Title", description="This is the title shown on the navigation bar"
+    )
+    route = StringField(
+        "Route",
+        description="This is the URL route that your page will be at (e.g. /page). You can also enter links to link to that page.",
+    )
+    draft = BooleanField("Draft")
+    hidden = BooleanField("Hidden")
+    auth_required = BooleanField("Authentication Required")
+    content = TextAreaField("Content")
 
 
 class PageFilesUploadForm(BaseForm):
