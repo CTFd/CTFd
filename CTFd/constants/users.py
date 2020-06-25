dea6b122b73db0295b5b896c102d41a7e4853951
Kevin Chung <kchung@ctfd.io>
2020-06-25 12:00:33 -0400

    Add User and Team Jinja globals to access User/Team attrs (#1511)
    * Adds User and Team globals to Jinja theme files. 
    * Closes #1234

--- CTFd/constants/users.py
+++ CTFd/constants/users.py
@@ -20,3 +20,32 @@ UserAttrs = namedtuple(
         "created",
     ],
 )
+
+
+class _UserAttrsWrapper:
+    def __getattr__(self, attr):
+        from CTFd.utils.user import get_current_user_attrs
+
+        attrs = get_current_user_attrs()
+        return getattr(attrs, attr, None)
+
+    @property
+    def place(self):
+        from CTFd.utils.user import get_current_user
+
+        user = get_current_user()
+        if user:
+            return user.place
+        return None
+
+    @property
+    def score(self):
+        from CTFd.utils.user import get_current_user
+
+        user = get_current_user()
+        if user:
+            return user.score
+        return None
+
+
+User = _UserAttrsWrapper()
