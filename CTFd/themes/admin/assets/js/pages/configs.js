58056e83e91dcae16ae59a4a54ca7d2d55340a1a
Kevin Chung <kchung@nyu.edu>
2020-09-02 03:30:21 -0400

    Add rough draft for TOS and privacy pages

--- CTFd/themes/admin/assets/js/pages/configs.js
+++ CTFd/themes/admin/assets/js/pages/configs.js
@@ -268,6 +268,15 @@ $(() => {
     theme_settings_editor.refresh();
   });
 
+  $("a[href='#legal']").on("shown.bs.tab", function(_e) {
+    $("#tos-config .CodeMirror").each(function(i, el) {
+      el.CodeMirror.refresh();
+    });
+    $("#privacy-policy-config .CodeMirror").each(function(i, el) {
+      el.CodeMirror.refresh();
+    });
+  });
+
   $("#theme-settings-modal form").submit(function(e) {
     e.preventDefault();
     theme_settings_editor
