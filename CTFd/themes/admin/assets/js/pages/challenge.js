cfde6c99afa8745ffeed15d0824554e7b6269dd0
Kevin Chung <kchung@ctfd.io>
2020-09-08 17:34:27 -0400

    Fix regression in challenge creation (#1638)
    * Closes #1637

--- CTFd/themes/admin/assets/js/pages/challenge.js
+++ CTFd/themes/admin/assets/js/pages/challenge.js
@@ -425,13 +425,17 @@ $(() => {
   $("#flags-create-select").change(flagTypeSelect);
   $(".edit-flag").click(editFlagModal);
 
-  // Insert CommentBox element
-  const commentBox = Vue.extend(CommentBox);
-  let vueContainer = document.createElement("div");
-  document.querySelector("#comment-box").appendChild(vueContainer);
-  new commentBox({
-    propsData: { type: "challenge", id: window.CHALLENGE_ID }
-  }).$mount(vueContainer);
+  // Because this JS is shared by a few pages,
+  // we should only insert the CommentBox if it's actually in use
+  if (document.querySelector("#comment-box")) {
+    // Insert CommentBox element
+    const commentBox = Vue.extend(CommentBox);
+    let vueContainer = document.createElement("div");
+    document.querySelector("#comment-box").appendChild(vueContainer);
+    new commentBox({
+      propsData: { type: "challenge", id: window.CHALLENGE_ID }
+    }).$mount(vueContainer);
+  }
 
   $.get(CTFd.config.urlRoot + "/api/v1/challenges/types", function(response) {
     const data = response.data;
