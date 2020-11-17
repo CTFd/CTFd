66ff9c0b91a92c0fbb24de30f690dd11d0dbe603
Kevin Chung <kchung@ctfd.io>
2020-11-16 20:24:42 -0500

    Add import export commands to manage.py (#1723)
    * Add `import_ctf` and `export_ctf` commands to `manage.py`
    * Deprecate `import.py` and `export.py`
    * Works on #1629

--- manage.py
+++ manage.py
@@ -1,10 +1,15 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+import datetime
+import shutil
+
+from flask_migrate import MigrateCommand
 from flask_script import Manager
-from flask_migrate import Migrate, MigrateCommand
+
 from CTFd import create_app
-from CTFd.utils import get_config as get_config_util, set_config as set_config_util
-from CTFd.models import *
+from CTFd.utils import get_config as get_config_util
+from CTFd.utils import set_config as set_config_util
+from CTFd.utils.config import ctf_name
+from CTFd.utils.exports import export_ctf as export_ctf_util
+from CTFd.utils.exports import import_ctf as import_ctf_util
 
 app = create_app()
 
@@ -44,6 +49,31 @@ def build(cmd):
     with app.app_context():
         cmd = BUILD_COMMANDS.get(cmd)
         cmd()
+
+
+@manager.command
+def export_ctf(path=None):
+    with app.app_context():
+        backup = export_ctf_util()
+
+        if path:
+            with open(path, "wb") as target:
+                shutil.copyfileobj(backup, target)
+        else:
+            name = ctf_name()
+            day = datetime.datetime.now().strftime("%Y-%m-%d")
+            full_name = f"{name}.{day}.zip"
+
+            with open(full_name, "wb") as target:
+                shutil.copyfileobj(backup, target)
+
+            print(f"Exported {full_name}")
+
+
+@manager.command
+def import_ctf(path):
+    with app.app_context():
+        import_ctf_util(path)
 
 
 if __name__ == "__main__":
