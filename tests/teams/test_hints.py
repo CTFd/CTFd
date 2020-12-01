461a51989b811cbb2fd53e591bd975d86ed3ea87
Kevin Chung <kchung@nyu.edu>
2020-12-01 02:57:30 -0500

    Change unlocking user

--- tests/teams/test_hints.py
+++ tests/teams/test_hints.py
@@ -27,7 +27,9 @@ def test_hint_team_unlock():
         team.members.append(second_user)
         chal = gen_challenge(app.db)
         gen_hint(app.db, chal.id, content="hint", cost=1, type="standard")
-        gen_award(app.db, 2, team.id)
+        # Give the points to the user that doesn't unlock
+        # Users that unlock hints should be able to unlock but cost their team points
+        gen_award(app.db, user_id=3, team_id=team.id)
         app.db.session.commit()
         with login_as_user(app, name="user_name") as client:
             # Assert that we don't see a hint
