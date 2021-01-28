7f115bf4581b8511e5a4a548e55f74d0beef6680
Kevin Chung <kchung@ctfd.io>
2021-01-28 16:55:15 -0500

    Add length error content that is too long (#1787)
    * Add length checking to some sensitive fields in Pages and Challenges.
    * Works on #1786
    
    This is enough to fix most of the issues but this is really a systemic problem for most of the API endpoints. We should have something that verifies data consistency. Marshmallow is not good enough at this. Pydantic seems like it would be superior here.

--- CTFd/schemas/pages.py
+++ CTFd/schemas/pages.py
@@ -1,4 +1,4 @@
-from marshmallow import pre_load
+from marshmallow import ValidationError, pre_load
 
 from CTFd.models import Pages, ma
 from CTFd.utils import string_types
@@ -11,10 +11,33 @@ class PageSchema(ma.ModelSchema):
         dump_only = ("id",)
 
     @pre_load
+    def validate_title(self, data):
+        title = data.get("title", "")
+        if len(title) > 128:
+            raise ValidationError(
+                "Page could not be saved. Your title is too long.",
+                field_names=["title"],
+            )
+
+    @pre_load
     def validate_route(self, data):
-        route = data.get("route")
-        if route and route.startswith("/"):
+        route = data.get("route", "")
+        if route.startswith("/"):
             data["route"] = route.strip("/")
+        if len(route) > 128:
+            raise ValidationError(
+                "Page could not be saved. Your route is too long.",
+                field_names=["route"],
+            )
+
+    @pre_load
+    def validate_content(self, data):
+        content = data.get("content", "")
+        if len(content) >= 65536:
+            raise ValidationError(
+                "Page could not be saved. Your content is too long.",
+                field_names=["content"],
+            )
 
     def __init__(self, view=None, *args, **kwargs):
         if view:
