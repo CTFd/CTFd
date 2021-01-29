8e7939ecedc143c6df43ddbc64c9d68d2072c34d
Frank <frankli0324@hotmail.com>
2021-01-29 14:39:49 +0800

    use field based schema

--- CTFd/schemas/challenges.py
+++ CTFd/schemas/challenges.py
@@ -1,4 +1,5 @@
-from marshmallow import ValidationError, pre_load
+from marshmallow import validate
+from marshmallow_sqlalchemy import field_for
 
 from CTFd.models import Challenges, ma
 
@@ -9,29 +10,27 @@ class ChallengeSchema(ma.ModelSchema):
         include_fk = True
         dump_only = ("id",)
 
-    @pre_load
-    def validate_name(self, data):
-        name = data.get("name", "")
-        if len(name) > 80:
-            raise ValidationError(
-                "Challenge could not be saved. Challenge name too long",
-                field_names=["name"],
-            )
+    name = field_for(
+        Challenges,
+        "name",
+        validate=[
+            validate.Length(min=0, max=80, error="Challenge could not be saved. Challenge name too long")
+        ],
+    )
 
-    @pre_load
-    def validate_category(self, data):
-        category = data.get("category", "")
-        if len(category) > 80:
-            raise ValidationError(
-                "Challenge could not be saved. Challenge category too long",
-                field_names=["category"],
-            )
+    category = field_for(
+        Challenges,
+        "category",
+        validate=[
+            validate.Length(min=0, max=80, error="Challenge could not be saved. Challenge category too long")
+        ]
+    )
 
-    @pre_load
-    def validate_description(self, data):
-        description = data.get("description", "")
-        if len(description) >= 65536:
-            raise ValidationError(
-                "Challenge could not be saved. Challenge description is too long.",
-                field_names=["description"],
-            )
+    description = field_for(
+        Challenges,
+        "description",
+        allow_none=True,
+        validate=[
+            validate.Length(min=0, max=65535, error="Challenge could not be saved. Challenge description too long")
+        ]
+    )
