a4ac654969bc5deeea1eb091c48005c521a90cc9
Kevin Chung <kchung@nyu.edu>
2021-01-28 23:18:55 -0500

    Pushing up some existing work

---
+++ CTFd/themes/admin/assets/js/components/teams/UserAddForm.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <div>
+      <input type="text">
+    </div>
+    <div>
+      <input type="text">
+    </div>
+  </div>
+</template>
+
+<script>
+import $ from "jquery";
+import CTFd from "core/CTFd";
+
+export default {
+  name: "UserAddForm",
+  props: {
+    team_id: Number
+  },
+  data: function() {
+    return {
+      users: [],
+    };
+  },
+  methods: {
+    searchUsers: function() {
+
+    },
+    addUsertoTeam: function() {
+
+    }
+  }
+};
+</script>
+
+<style scoped></style>
