e5dbd62a66d3c47f2288f42888fa44bafeef454a
Kevin Chung <kchung@ctfd.io>
2021-03-16 19:03:55 -0400

    Fix frontend UI where empty/null requirements could be added (#1824)
    * Fix Challenge Requirements interface in Admin Panel to not allow empty/null requirements to be added
    * Closes #1809

--- CTFd/schemas/challenges.py
+++ CTFd/schemas/challenges.py
@@ -1,7 +1,27 @@
 from marshmallow import validate
+from marshmallow.exceptions import ValidationError
 from marshmallow_sqlalchemy import field_for
 
 from CTFd.models import Challenges, ma
+
+
+class ChallengeRequirementsValidator(validate.Validator):
+    default_message = "Error parsing challenge requirements"
+
+    def __init__(self, error=None):
+        self.error = error or self.default_message
+
+    def __call__(self, value):
+        if isinstance(value, dict) is False:
+            raise ValidationError(self.default_message)
+
+        prereqs = value.get("prerequisites", [])
+        if all(prereqs) is False:
+            raise ValidationError(
+                "Challenge requirements cannot have a null prerequisite"
+            )
+
+        return value
 
 
 class ChallengeSchema(ma.ModelSchema):
@@ -45,4 +65,8 @@ class ChallengeSchema(ma.ModelSchema):
                 error="Challenge could not be saved. Challenge description too long",
             )
         ],
+    )
+
+    requirements = field_for(
+        Challenges, "requirements", validate=[ChallengeRequirementsValidator()],
     )
