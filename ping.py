99da88fd2479dd322980553c1119e264bded3592
Frank <frankli0324@hotmail.com>
2021-04-14 23:09:00 +0800

    back to using raw_connection

--- ping.py
+++ ping.py
@@ -1,5 +1,6 @@
 import time
 
+from sqlalchemy import create_engine
 from sqlalchemy.engine.url import make_url
 
 from CTFd.config import Config
@@ -7,41 +8,17 @@ from CTFd.config import Config
 url = make_url(Config.DATABASE_URL)
 url.database = None
 
-
-class PingError(Exception):
-    pass
-
-
-def mysql_ping(**kwargs):
-    """this imitates `mysqladmin ping`"""
-    from pymysql import Error, connect
-
-    try:
-        connect(**kwargs).ping()
-    except Error:
-        raise PingError()
-
-
-def postgres_ping(**kwargs):
-    """NotImplemented"""
-
+if url.drivername.startswith('sqlite'):
+    exit(0)
+engine = create_engine(url)
 
 print(f"Waiting for {url} to be ready")
 
 while True:
     try:
-        if url.drivername.startswith("mysql"):
-            mysql_ping(
-                host=url.host,
-                user=url.username,
-                password=url.password,
-                port=url.port,
-                database=None,
-            )
-        elif url.drivername.startswith("postgres"):
-            postgres_ping()
+        engine.raw_connection()
         break
-    except PingError:
+    except Exception:
         print(".", end="", flush=True)
         time.sleep(1)
 
