e4d0c4f431855b61ec481bbc956bfa0a05dc31db
Frank <frankli0324@hotmail.com>
2021-04-14 20:48:41 +0800

    Update ping.py

--- ping.py
+++ ping.py
@@ -1,26 +1,49 @@
-import os
 import time
 
-from sqlalchemy import create_engine
 from sqlalchemy.engine.url import make_url
 
 from CTFd.config import Config
 
 url = make_url(Config.DATABASE_URL)
-if not url.drivername.startswith("mysql"):
-    exit(0)
+url.database = None
 
-engine = create_engine(url)
-print(f"Waiting for {url.host}:{url.port or 3306} to be ready")
+
+class PingError(Exception):
+    pass
+
+
+def mysql_ping(**kwargs):
+    """this imitates `mysqladmin ping`"""
+    from pymysql import Error, connect
+    try:
+        connect(**kwargs).ping()
+    except Error:
+        raise PingError()
+
+
+def postgres_ping(**kwargs):
+    """NotImplemented"""
+
+
+print(f"Waiting for {url} to be ready")
 
 while True:
     try:
-        engine.raw_connection().ping()
+        if url.drivername.startswith("mysql"):
+            mysql_ping(
+                host=url.host,
+                user=url.username,
+                password=url.password,
+                port=url.port,
+                database=None,
+            )
+        elif url.drivername.startswith("postgres"):
+            postgres_ping()
         print()
         break
-    except:
+    except PingError:
         print(".", end="", flush=True)
         time.sleep(1)
 
-print(f"{url.host}:{url.port or 3306} is ready")
+print(f"{url} is ready")
 time.sleep(1)
