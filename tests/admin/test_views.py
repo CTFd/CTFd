44e1ba0da1f641bcf06ca4cf4253a56e0c02c261
Ife <il693@nyu.edu>
2021-06-02 13:20:47 -0400

    test coverage for the admin/users endpoint in teams mode

--- tests/admin/test_views.py
+++ tests/admin/test_views.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from CTFd.utils.modes import TEAMS_MODE
+from CTFd.models import Users
 from flask import Flask
+from CTFd.utils import set_config
 
 from tests.helpers import (
     create_ctfd,
@@ -11,6 +14,7 @@ from tests.helpers import (
     gen_team,
     login_as_user,
     register_user,
+    register_team
 )
 
 
@@ -79,4 +83,21 @@ def test_get_admin_as_user():
         r = client.get("/admin")
         assert r.status_code == 302
         assert r.location.startswith("http://localhost/login")
+    destroy_ctfd(app)
+
+
+def test_admin_view_user_team_score():
+    app = create_ctfd(user_mode="teams")
+    with app.app_context():
+        register_user(app)
+        team = gen_team(app.db)
+        user = Users.query.filter_by(id=2).first()
+        user.team_id = team.id
+        app.db.session.commit()
+
+        with login_as_user(app, name="admin") as admin:
+            # Coverage of an admin seeing users in team mode
+            r = admin.get("/admin/users/2")
+            assert r.status_code == 200
+
     destroy_ctfd(app)
