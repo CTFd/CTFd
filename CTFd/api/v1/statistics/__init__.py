fd8fbce55dffedbe892aca0b746a5932501c4610
Ife <il693@nyu.edu>
2021-06-03 11:39:56 -0400

    made format changes and edited stats to correct for linting issues

--- CTFd/api/v1/statistics/__init__.py
+++ CTFd/api/v1/statistics/__init__.py
@@ -1,7 +1,3 @@
-statistics_namespace = Namespace(
-    "statistics", description="Endpoint to retrieve Statistics"
-)
-
 # isort:imports-firstparty
 from CTFd.api.v1.statistics import challenges  # noqa: F401
 from CTFd.api.v1.statistics import scores  # noqa: F401
@@ -10,3 +6,7 @@ from CTFd.api.v1.statistics import teams  # noqa: F401
 from CTFd.api.v1.statistics import users  # noqa: F401
 
 from flask_restx import Namespace
+
+statistics_namespace = Namespace(
+    "statistics", description="Endpoint to retrieve Statistics"
+)
