a29806b425ec933d0715984a66551c659b3b9671
Kevin Chung <kchung@nyu.edu>
2021-07-01 20:52:57 -0400

    Add a fix for receiving non-string Config values

--- CTFd/schemas/config.py
+++ CTFd/schemas/config.py
@@ -1,8 +1,23 @@
-from marshmallow import validate
+from marshmallow import fields
+from marshmallow.exceptions import ValidationError
 from marshmallow_sqlalchemy import field_for
 
 from CTFd.models import Configs, ma
 from CTFd.utils import string_types
+
+
+class ConfigValueField(fields.Field):
+    """
+    Custom value field for Configs so that we can perform validation of values
+    """
+
+    def _deserialize(self, value, attr, data, **kwargs):
+        if isinstance(value, str):
+            if len(value) > 65535:
+                raise ValidationError(f'{data["key"]} config is too long')
+            return value
+        else:
+            return value
 
 
 class ConfigSchema(ma.ModelSchema):
@@ -13,11 +28,7 @@ class ConfigSchema(ma.ModelSchema):
 
     views = {"admin": ["id", "key", "value"]}
     key = field_for(Configs, "key", required=True)
-    value = field_for(
-        Configs,
-        "value",
-        validate=[validate.Length(max=64000, error="Config is too long")],
-    )
+    value = ConfigValueField(allow_none=True)
 
     def __init__(self, view=None, *args, **kwargs):
         if view:
