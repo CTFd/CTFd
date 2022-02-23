1e06ec6fa50c467ac418b0125eb54a991675e256
smidge <smdigedy@gmail.com>
2022-02-23 22:45:21 +1100

    post deploy fix

--- post-deploy.sh
+++ post-deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-
+#
 # create DB URL - grab from the JAWSDB configured URL
-heroku config:set DATABASE_URL=`echo ${JAWSDB_MARIA_URL} | sed 's/mysql\:\/\//mysql+pymysql\:\/\//'`
-
+env DATABASE_URL=`echo ${JAWSDB_MARIA_URL} | sed 's/mysql\:\/\//mysql+pymysql\:\/\//'`
+#
 # create REDIS URL - grab from REDISTOGO
-heroku config:set REDIS_URL=`echo ${REDISTOGO_URL}`
+env REDIS_URL=`echo ${REDISTOGO_URL}`
