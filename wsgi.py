fe4e2f226b5d1e60dedc86b6b9bf4cbe3dfe6750
smidge <smdigedy@gmail.com>
2022-02-23 21:37:29 +1100

    heroku

--- wsgi.py
+++ wsgi.py
@@ -11,4 +11,4 @@ from CTFd import create_app
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(debug=True, threaded=True, host="127.0.0.1", port=80)
+    app.run(debug=True, threaded=True, host="127.0.0.1", port=4000)
