7977bf2d67f9a9943dafc12a5e895fdb771949a3
smidge <smdigedy@gmail.com>
2022-02-23 22:28:49 +1100

    improving app.json

---
+++ post-deploy.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# create DB URL - grab from the JAWSDB configured URL
+heroku config:set DATABASE_URL=`echo ${JAWSDB_MARIA_URL} | sed 's/mysql\:\/\//mysql+pymysql\:\/\//'`
+
+# create REDIS URL - grab from REDISTOGO
+heroku config:set REDIS_URL=`echo ${REDISTOGO_URL}`
