a4b0761231bd6f3b234f7b692ae138bea701cea7
smidge <smdigedy@gmail.com>
2022-02-24 00:53:36 +1100

    yoooo

--- deploy-heroku.sh
+++ deploy-heroku.sh
@@ -3,14 +3,6 @@
 # Pre-requisites
 # - You've got heroku CLI installed somehow
 
-
-# create DB URL - grab from the JAWSDB configured URL
-#NEW_URL=`echo ${JAWSDB_MARIA_URL} | sed 's/mysql\:\/\//mysql+pymysql\:\/\//'`
-#export DATABASE_URL="${NEW_URL}"
-
-# create REDIS URL - grab from REDISTOGO
-#export REDIS_URL="${REDISTOGO_URL}"
-
 heroku login
 heroku apps:create --region us
 git push heroku main
