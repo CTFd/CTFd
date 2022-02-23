867bd0321083791487bcd09285795261b6626c0c
smidge <smdigedy@gmail.com>
2022-02-24 00:53:22 +1100

    yoooo

--- deploy-heroku.sh
+++ deploy-heroku.sh
@@ -29,6 +29,8 @@ heroku config:set DATABASE_URL="${NEW_DB_URL}"
 REDIS_URL=`heroku config:get REDISTOGO_URL`
 heroku config:set REDIS_URL="${REDIS_URL}"
 
-heroku ps:restart
-
+echo "\n\n\n\n\n"
+echo "Job's done - app is at the link below. After initial deploy, let the app sit for 10 minutes or so while it shits itself. Should start working after that."
 echo "https://${APP_NAME}.herokuapp.com"
+echo "When you're ready to start paying for scalabiltiy, change the dyno type in the heroku panel and also consider upgrading the DB and redis addon."
+echo "Maybe chuck it behind a CDN like cloudflare on free tier."
