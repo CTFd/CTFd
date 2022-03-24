550fa6f7fcf9d563f309bf541d4b8ca32ad5d5b7
Markus Ankenbrand <markus@ankenbrand.me>
2022-03-24 14:50:51 +0100

    Add mape flag

--- CTFd/plugins/flags/__init__.py
+++ CTFd/plugins/flags/__init__.py
@@ -93,7 +93,32 @@ class CTFdMSEFlag(BaseFlag):
         mse /= len(provided_np)
         return mse < float(data)
 
-FLAG_CLASSES = {"static": CTFdStaticFlag, "regex": CTFdRegexFlag, "mse": CTFdMSEFlag}
+class CTFdMAPEFlag(BaseFlag):
+    name = "mape"
+    templates = {
+        "create": "/plugins/flags/assets/mape/create.html",
+        "update": "/plugins/flags/assets/mape/edit.html",
+    }
+
+    @staticmethod
+    def compare(chal_key_obj, provided):
+        saved = chal_key_obj.content
+        data = chal_key_obj.data
+        try:
+            provided_np = [float(x) for x in provided.split(",")]
+            saved_np = [float(x) for x in saved.split(",")]
+        except ValueError:
+            raise FlagException("Flag must be comma separated floats")
+        if len(provided_np) != len(saved_np):
+            raise FlagException(f"Incorrect number of values: provided {len(provided_np)}, expected {len(saved_np)}")
+        mape = 0
+        for p,s in zip(provided_np, saved_np):
+            mape += abs(p-s)/s
+        mape /= len(provided_np)
+        mape *= 100
+        return mape < float(data)
+
+FLAG_CLASSES = {"static": CTFdStaticFlag, "regex": CTFdRegexFlag, "mse": CTFdMSEFlag, "mape": CTFdMAPEFlag}
 
 
 def get_flag_class(class_id):
