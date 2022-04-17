9e71ed960af266a6b07eafd3d88e29ed87985b86
Kevin Chung <kchung@ctfd.io>
2022-04-17 15:34:42 -0400

    Fix lint

--- CTFd/utils/exports/databases.py
+++ CTFd/utils/exports/databases.py
@@ -1,10 +1,11 @@
 from CTFd.models import db
+from sqlalchemy.exc import OperationalError
 
 
 def is_database_mariadb():
     try:
         result = db.session.execute("SELECT version()").fetchone()[0]
         mariadb = "mariadb" in result.lower()
-    except:
+    except OperationalError:
         mariadb = False
     return mariadb
