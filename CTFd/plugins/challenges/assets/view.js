f3e43d97a6022a61a54c4d97d2166f3521febab0
Kevin Chung <kchung@ctfd.io>
2022-04-30 22:05:16 -0400

    Null out some functions in view.js (#2105)
    * Remove some unused functions from the default provided standard & dynamic challenge `view.js` files

--- CTFd/plugins/challenges/assets/view.js
+++ CTFd/plugins/challenges/assets/view.js
@@ -1,40 +1,37 @@
-CTFd._internal.challenge.data = undefined
+CTFd._internal.challenge.data = undefined;
 
-CTFd._internal.challenge.renderer = CTFd.lib.markdown();
+// TODO: Remove in CTFd v4.0
+CTFd._internal.challenge.renderer = null;
 
+CTFd._internal.challenge.preRender = function() {};
 
-CTFd._internal.challenge.preRender = function () { }
+// TODO: Remove in CTFd v4.0
+CTFd._internal.challenge.render = null;
 
-CTFd._internal.challenge.render = function (markdown) {
-    return CTFd._internal.challenge.renderer.render(markdown)
-}
+CTFd._internal.challenge.postRender = function() {};
 
+CTFd._internal.challenge.submit = function(preview) {
+  var challenge_id = parseInt(CTFd.lib.$("#challenge-id").val());
+  var submission = CTFd.lib.$("#challenge-input").val();
 
-CTFd._internal.challenge.postRender = function () { }
+  var body = {
+    challenge_id: challenge_id,
+    submission: submission
+  };
+  var params = {};
+  if (preview) {
+    params["preview"] = true;
+  }
 
-
-CTFd._internal.challenge.submit = function (preview) {
-    var challenge_id = parseInt(CTFd.lib.$('#challenge-id').val())
-    var submission = CTFd.lib.$('#challenge-input').val()
-
-    var body = {
-        'challenge_id': challenge_id,
-        'submission': submission,
+  return CTFd.api.post_challenge_attempt(params, body).then(function(response) {
+    if (response.status === 429) {
+      // User was ratelimited but process response
+      return response;
     }
-    var params = {}
-    if (preview) {
-        params['preview'] = true
+    if (response.status === 403) {
+      // User is not logged in or CTF is paused.
+      return response;
     }
-
-    return CTFd.api.post_challenge_attempt(params, body).then(function (response) {
-        if (response.status === 429) {
-            // User was ratelimited but process response
-            return response
-        }
-        if (response.status === 403) {
-            // User is not logged in or CTF is paused.
-            return response
-        }
-        return response
-    })
+    return response;
+  });
 };
