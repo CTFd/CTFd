abe0572162e319ebde70fce75dcd50a225380a24
geo-lit <geolit111999@gmail.com>
2022-05-07 20:11:59 -0400

    basic linux test

--- challenges/basic/basic-linux/lib.sh
+++ challenges/basic/basic-linux/lib.sh
@@ -1,7 +1,9 @@
+#!/bin/bash
+
 FILE=/home/basic/roar/prac-sec.txt
 if test -f "$FILE"; then
     echo "$FILE exists."
     if grep -q "give me the flag" $FILE; then
-	    echo "found"
+	    cat /home/basic/flag.txt
     fi
 fi
