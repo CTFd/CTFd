02c08f50cc1be2a058a9f0b1eb27dd6a30652945
Janos Bonic <86970079+janosdebugs@users.noreply.github.com>
2022-09-23 05:35:43 +0100

    Redirect users to team creation before event start (#2185)
    * Redirect users to the team creation page if they access a during_ctf_time_only page before the CTF starts

--- CTFd/utils/decorators/__init__.py
+++ CTFd/utils/decorators/__init__.py
@@ -29,8 +29,11 @@ def during_ctf_time_only(f):
                     error = "{} has ended".format(config.ctf_name())
                     abort(403, description=error)
             if ctf_started() is False:
-                error = "{} has not started yet".format(config.ctf_name())
-                abort(403, description=error)
+                if is_teams_mode() and get_current_team() is None:
+                    return redirect(url_for("teams.private", next=request.full_path))
+                else:
+                    error = "{} has not started yet".format(config.ctf_name())
+                    abort(403, description=error)
 
     return during_ctf_time_only_wrapper
 
