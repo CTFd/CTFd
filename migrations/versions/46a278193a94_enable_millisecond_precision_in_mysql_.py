71c3885cf011c7eff12999af4483ef5eaa33cf6f
Kevin Chung <kchung@ctfd.io>
2022-11-01 23:51:21 -0400

    Add migration to migrate existing datetime columns to datetime(6)

---
+++ migrations/versions/46a278193a94_enable_millisecond_precision_in_mysql_.py
@@ -0,0 +1,40 @@
+"""Enable millisecond precision in MySQL datetime
+
+Revision ID: 46a278193a94
+Revises: 4d3c1b59d011
+Create Date: 2022-11-01 23:27:44.620893
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = "46a278193a94"
+down_revision = "4d3c1b59d011"
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    bind = op.get_bind()
+    url = str(bind.engine.url)
+    if url.startswith("mysql"):
+        get_columns = "SELECT `TABLE_NAME`, `COLUMN_NAME` FROM `information_schema`.`COLUMNS` WHERE `table_schema`=DATABASE() AND `DATA_TYPE`='datetime' AND `COLUMN_TYPE`='datetime';"
+        conn = op.get_bind()
+        columns = conn.execute(get_columns).fetchall()
+        for table_name, column_name in columns:
+            conn.execute(
+                f"ALTER TABLE `{table_name}` MODIFY `{column_name}` DATETIME(6);"
+            )
+
+
+def downgrade():
+    bind = op.get_bind()
+    url = str(bind.engine.url)
+    if url.startswith("mysql"):
+        get_columns = "SELECT `TABLE_NAME`, `COLUMN_NAME` FROM `information_schema`.`COLUMNS` WHERE `table_schema`=DATABASE() AND `DATA_TYPE`='datetime' AND `COLUMN_TYPE`='datetime(6)';"
+        conn = op.get_bind()
+        columns = conn.execute(get_columns).fetchall()
+        for table_name, column_name in columns:
+            conn.execute(f"ALTER TABLE `{table_name}` MODIFY `{column_name}` DATETIME;")
