f1cd5c58033b3d83227e86b87314b259eda2b0ca
Frank <frankli0324@hotmail.com>
2022-11-12 12:15:45 +0800

    make linter happy

--- CTFd/exceptions/__init__.py
+++ CTFd/exceptions/__init__.py
@@ -18,8 +18,8 @@ class PluginDependencyException(Exception):
     def __init__(self, dependencies: list, *args):
         super().__init__(*args)
         if (
-            isinstance(self.dependencies, list) and
-            all((isinstance(d, str) for d in dependencies))
+            isinstance(self.dependencies, list)
+            and all((isinstance(d, str) for d in dependencies))
         ):
             self.dependencies = dependencies
         else:
