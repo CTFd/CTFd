e9de1d708e84b58e15e724a323715048a833acdb
Kevin Chung <kchung@ctfd.io>
2023-06-01 15:53:56 -0400

    Squashed 'CTFd/themes/core-beta/' content from commit 9126d77d
    git-subtree-dir: CTFd/themes/core-beta
    git-subtree-split: 9126d77d1a858e10c25ce028a35d42efaa0f49c0

---
+++ assets/js/utils/graphs/echarts/index.js
@@ -0,0 +1,44 @@
+import * as echarts from "echarts/core";
+import { LineChart } from "echarts/charts";
+import {
+  TitleComponent,
+  TooltipComponent,
+  GridComponent,
+  DatasetComponent,
+  TransformComponent,
+  LegendComponent,
+  ToolboxComponent,
+  DataZoomComponent,
+} from "echarts/components";
+// Features like Universal Transition and Label Layout
+import { LabelLayout, UniversalTransition } from "echarts/features";
+// Import the Canvas renderer
+// Note that introducing the CanvasRenderer or SVGRenderer is a required step
+import { CanvasRenderer } from "echarts/renderers";
+
+// Register the required components
+echarts.use([
+  LineChart,
+  TitleComponent,
+  TooltipComponent,
+  GridComponent,
+  DatasetComponent,
+  TransformComponent,
+  LegendComponent,
+  ToolboxComponent,
+  DataZoomComponent,
+  LabelLayout,
+  UniversalTransition,
+  CanvasRenderer,
+]);
+
+export function embed(target, option) {
+  let chart = echarts.init(target);
+  chart.setOption(option);
+
+  window.addEventListener("resize", () => {
+    if (chart) {
+      chart.resize();
+    }
+  });
+}
---
+++ static/assets/scoreboard.a8a2da7f.js
@@ -0,0 +1,1 @@
+import{m as t,C as a}from"./index.1d7c7e9f.js";import{g as e,e as i}from"./index.159418f4.js";import"./echarts.128204f2.js";window.Alpine=t;window.CTFd=a;t.data("ScoreboardDetail",()=>({data:null,async init(){this.data=await a.pages.scoreboard.getScoreboardDetail(10);let o=e(a.config.userMode,this.data);i(this.$refs.scoregraph,o)}}));t.start();
