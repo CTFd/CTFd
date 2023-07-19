70b26b06d8307c8594b1169ee0b116b62a3d7880
Kevin Chung <kchung@ctfd.io>
2023-07-19 00:33:29 -0400

    Fix translations (#2371)
    * Fix existing translations and add Spanish and Chinese
    * Closes #2366

--- CTFd/constants/languages.py
+++ CTFd/constants/languages.py
@@ -5,12 +5,16 @@ class Languages(str, RawEnum):
     ENGLISH = "en"
     GERMAN = "de"
     POLISH = "pl"
+    SPANISH = "es"
+    CHINESE = "zh"
 
 
 LANGUAGE_NAMES = {
     "en": "English",
     "de": "Deutsch",
     "pl": "Polski",
+    "es": "Español",
+    "zh": "中文",
 }
 
 SELECT_LANGUAGE_LIST = [("", "")] + [
