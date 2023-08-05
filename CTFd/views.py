edbbc5f9a804a6a93928c87fa6601a02e31dec0f
Kevin Chung <kchung@ctfd.io>
2023-08-05 04:16:38 -0400

    Downgrade Werkzeug from 2.1.2 to 2.0.3 to avoid removal errors (#2384)
    * Downgrade Werkzeug from 2.1.2 to 2.0.3 to avoid removal errors
    * Fix warnings from Werkzeug migration from v1 to v2

--- CTFd/views.py
+++ CTFd/views.py
@@ -11,9 +11,9 @@ from flask import (
     session,
     url_for,
 )
-from flask.helpers import safe_join
 from jinja2.exceptions import TemplateNotFound
 from sqlalchemy.exc import IntegrityError
+from werkzeug.utils import safe_join
 
 from CTFd.cache import cache
 from CTFd.constants.config import (
@@ -492,6 +492,9 @@ def themes(theme, path):
         # admin pages, so we check that first
         for cand_theme in (theme, *config.ctf_theme_candidates())
     ):
+        # Handle werkzeug behavior of returning None on malicious paths
+        if cand_path is None:
+            abort(404)
         if os.path.isfile(cand_path):
             return send_file(cand_path)
     abort(404)
@@ -512,6 +515,9 @@ def themes_beta(theme, path):
         # admin pages, so we check that first
         for cand_theme in (theme, *config.ctf_theme_candidates())
     ):
+        # Handle werkzeug behavior of returning None on malicious paths
+        if cand_path is None:
+            abort(404)
         if os.path.isfile(cand_path):
             return send_file(cand_path)
     abort(404)
