2ba501eb025a31c8a46d72a2404d5b8a342f7d1e
Kevin Chung <kchung@ctfd.io>
2023-08-10 18:22:40 -0400

    Make dynamic challenge migrations resilient to being applied twice (#2386)
    * Make dynamic challenge migrations resilient to being applied twice

--- CTFd/plugins/dynamic_challenges/migrations/b37fb68807ea_add_cascading_delete_to_dynamic_.py
+++ CTFd/plugins/dynamic_challenges/migrations/b37fb68807ea_add_cascading_delete_to_dynamic_.py
@@ -5,7 +5,8 @@ Revises:
 Create Date: 2020-05-06 12:21:39.373983
 
 """
-# revision identifiers, used by Alembic.
+import sqlalchemy
+
 revision = "b37fb68807ea"
 down_revision = None
 branch_labels = None
@@ -15,31 +16,43 @@ depends_on = None
 def upgrade(op=None):
     bind = op.get_bind()
     url = str(bind.engine.url)
-    if url.startswith("mysql"):
-        op.drop_constraint(
-            "dynamic_challenge_ibfk_1", "dynamic_challenge", type_="foreignkey"
-        )
-    elif url.startswith("postgres"):
-        op.drop_constraint(
-            "dynamic_challenge_id_fkey", "dynamic_challenge", type_="foreignkey"
-        )
 
-    op.create_foreign_key(
-        None, "dynamic_challenge", "challenges", ["id"], ["id"], ondelete="CASCADE"
-    )
-    # ### end Alembic commands ###
+    try:
+        if url.startswith("mysql"):
+            op.drop_constraint(
+                "dynamic_challenge_ibfk_1", "dynamic_challenge", type_="foreignkey"
+            )
+        elif url.startswith("postgres"):
+            op.drop_constraint(
+                "dynamic_challenge_id_fkey", "dynamic_challenge", type_="foreignkey"
+            )
+    except sqlalchemy.exc.InternalError as e:
+        print(str(e))
+
+    try:
+        op.create_foreign_key(
+            None, "dynamic_challenge", "challenges", ["id"], ["id"], ondelete="CASCADE"
+        )
+    except sqlalchemy.exc.InternalError as e:
+        print(str(e))
 
 
 def downgrade(op=None):
     bind = op.get_bind()
     url = str(bind.engine.url)
-    if url.startswith("mysql"):
-        op.drop_constraint(
-            "dynamic_challenge_ibfk_1", "dynamic_challenge", type_="foreignkey"
-        )
-    elif url.startswith("postgres"):
-        op.drop_constraint(
-            "dynamic_challenge_id_fkey", "dynamic_challenge", type_="foreignkey"
-        )
+    try:
+        if url.startswith("mysql"):
+            op.drop_constraint(
+                "dynamic_challenge_ibfk_1", "dynamic_challenge", type_="foreignkey"
+            )
+        elif url.startswith("postgres"):
+            op.drop_constraint(
+                "dynamic_challenge_id_fkey", "dynamic_challenge", type_="foreignkey"
+            )
+    except sqlalchemy.exc.InternalError as e:
+        print(str(e))
 
-    op.create_foreign_key(None, "dynamic_challenge", "challenges", ["id"], ["id"])
+    try:
+        op.create_foreign_key(None, "dynamic_challenge", "challenges", ["id"], ["id"])
+    except sqlalchemy.exc.InternalError as e:
+        print(str(e))
