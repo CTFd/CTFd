7150614dcb0533d44eeb5f5aeed860c6d4c9ae6f
Peter van Eijk <peter@peterhjvaneijk.nl>
2023-12-03 15:39:48 +0100

    fix: make black

--- CTFd/plugins/flags/tests/test_flags.py
+++ CTFd/plugins/flags/tests/test_flags.py
@@ -2,32 +2,33 @@ from CTFd.plugins.flags import CTFdRegexFlag
 
 
 def test_valid_regex_match_case_sensitive():
-    '''
+    """
     Test a valid regex match in a case-sensitive manner using CTFdRegexFlag
-    '''
+    """
     flag = CTFdRegexFlag()
-    flag.content = r'^[A-Z]\d{3}$'
-    flag.data = 'case_sensitive'
-    provided_flag = 'A123'
+    flag.content = r"^[A-Z]\d{3}$"
+    flag.data = "case_sensitive"
+    provided_flag = "A123"
     assert flag.compare(flag, provided_flag)
+
 
 def test_valid_regex_match_case_insensitive():
-    '''
+    """
     Test a valid regex match in a case-insensitive manner using CTFdRegexFlag
-    '''
+    """
     flag = CTFdRegexFlag()
-    flag.content = r'^[a-z]\d{3}$'
-    flag.data = 'case_insensitive'
-    provided_flag = 'A123'
+    flag.content = r"^[a-z]\d{3}$"
+    flag.data = "case_insensitive"
+    provided_flag = "A123"
     assert flag.compare(flag, provided_flag)
 
-def test_invalid_regex_match():
-    '''
-    Test an invalid regex match using CTFdRegexFlag
-    '''
-    flag = CTFdRegexFlag()
-    flag.content = r'^[A-Z]\d{3}$'
-    flag.data = 'case_sensitive'
-    provided_flag = 'invalid'
-    assert not flag.compare(flag, provided_flag)
 
+def test_invalid_regex_match():
+    """
+    Test an invalid regex match using CTFdRegexFlag
+    """
+    flag = CTFdRegexFlag()
+    flag.content = r"^[A-Z]\d{3}$"
+    flag.data = "case_sensitive"
+    provided_flag = "invalid"
+    assert not flag.compare(flag, provided_flag)
