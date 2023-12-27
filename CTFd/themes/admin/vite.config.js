53272a4b1278ffdb1747860c66891ba8aac73af1
Kevin Chung <kchung@ctfd.io>
2023-12-27 22:23:45 +0000

    Migrate fonts to admin theme

--- CTFd/themes/admin/vite.config.js
+++ CTFd/themes/admin/vite.config.js
@@ -9,9 +9,9 @@ export default defineConfig({
   resolve: {
     alias: {
       "~": resolve(__dirname, "./node_modules/"),
-      "core": resolve(__dirname, "../core/assets/js/"),
-      "vue": '@vue/compat',
-    },
+      core: resolve(__dirname, "../core/assets/js/"),
+      vue: "@vue/compat"
+    }
   },
   build: {
     manifest: true,
@@ -21,55 +21,65 @@ export default defineConfig({
         copy({
           targets: [
             // https://github.com/vitejs/vite/issues/1618#issuecomment-764579557
-            {
-              src: "./node_modules/@fortawesome/fontawesome-free/webfonts/**/*",
-              dest: "static/webfonts",
-            },
+            // {
+            //   src: "./node_modules/@fortawesome/fontawesome-free/webfonts/**/*",
+            //   dest: "static/webfonts",
+            // },
             {
               src: "./node_modules/@fontsource/lato/files/**/*400*-normal*",
-              dest: "static/webfonts",
+              dest: "static/webfonts"
             },
             {
               src: "./node_modules/@fontsource/lato/files/**/*700*-normal*",
-              dest: "static/webfonts",
+              dest: "static/webfonts"
             },
             {
-              src: "./node_modules/@fontsource/raleway/files/**/*400*-normal*",
-              dest: "static/webfonts",
+              src: "./node_modules/@fontsource/raleway/files/**/*500*-normal*",
+              dest: "static/webfonts"
             },
             {
               src: "./assets/img/**",
-              dest: "static/img",
-            },
+              dest: "static/img"
+            }
           ],
-          hook: "writeBundle",
-        }),
+          hook: "writeBundle"
+        })
       ],
       output: {
         manualChunks: {
-          echarts: ["echarts", "zrender"],
-        },
+          echarts: ["echarts", "zrender"]
+        }
       },
       input: {
-        'pages/main': resolve(__dirname, 'assets/js/pages/main.js'),
-        'pages/challenge': resolve(__dirname, 'assets/js/pages/challenge.js'),
-        'pages/challenges': resolve(__dirname, 'assets/js/pages/challenges.js'),
-        'pages/configs': resolve(__dirname, 'assets/js/pages/configs.js'),
-        'pages/notifications': resolve(__dirname, 'assets/js/pages/notifications.js'),
-        'pages/editor': resolve(__dirname, 'assets/js/pages/editor.js'),
-        'pages/pages': resolve(__dirname, 'assets/js/pages/pages.js'),
-        'pages/reset': resolve(__dirname, 'assets/js/pages/reset.js'),
-        'pages/scoreboard': resolve(__dirname, 'assets/js/pages/scoreboard.js'),
-        'pages/statistics': resolve(__dirname, 'assets/js/pages/statistics.js'),
-        'pages/submissions': resolve(__dirname, 'assets/js/pages/submissions.js'),
-        'pages/team': resolve(__dirname, 'assets/js/pages/team.js'),
-        'pages/teams': resolve(__dirname, 'assets/js/pages/teams.js'),
-        'pages/user': resolve(__dirname, 'assets/js/pages/user.js'),
-        'pages/users': resolve(__dirname, 'assets/js/pages/users.js'),
-        'admin-css': resolve(__dirname, 'assets/css/admin.scss'),
-        'codemirror-css': resolve(__dirname, 'assets/css/codemirror.scss'),
-        'challenge-board-css': resolve(__dirname, 'assets/css/challenge-board.scss'),
-      },
-    },
-  },
+        "pages/main": resolve(__dirname, "assets/js/pages/main.js"),
+        "pages/challenge": resolve(__dirname, "assets/js/pages/challenge.js"),
+        "pages/challenges": resolve(__dirname, "assets/js/pages/challenges.js"),
+        "pages/configs": resolve(__dirname, "assets/js/pages/configs.js"),
+        "pages/notifications": resolve(
+          __dirname,
+          "assets/js/pages/notifications.js"
+        ),
+        "pages/editor": resolve(__dirname, "assets/js/pages/editor.js"),
+        "pages/pages": resolve(__dirname, "assets/js/pages/pages.js"),
+        "pages/reset": resolve(__dirname, "assets/js/pages/reset.js"),
+        "pages/scoreboard": resolve(__dirname, "assets/js/pages/scoreboard.js"),
+        "pages/statistics": resolve(__dirname, "assets/js/pages/statistics.js"),
+        "pages/submissions": resolve(
+          __dirname,
+          "assets/js/pages/submissions.js"
+        ),
+        "pages/team": resolve(__dirname, "assets/js/pages/team.js"),
+        "pages/teams": resolve(__dirname, "assets/js/pages/teams.js"),
+        "pages/user": resolve(__dirname, "assets/js/pages/user.js"),
+        "pages/users": resolve(__dirname, "assets/js/pages/users.js"),
+        "fonts-css": resolve(__dirname, "assets/css/fonts.scss"),
+        "admin-css": resolve(__dirname, "assets/css/admin.scss"),
+        "codemirror-css": resolve(__dirname, "assets/css/codemirror.scss"),
+        "challenge-board-css": resolve(
+          __dirname,
+          "assets/css/challenge-board.scss"
+        )
+      }
+    }
+  }
 });
