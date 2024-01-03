7c91500a95e5ca566bdf1e0987010d7d553ef7d8
Kevin Chung <kchung@ctfd.io>
2024-01-03 13:50:26 -0500

    Add ability to disable social links

--- CTFd/api/v1/share.py
+++ CTFd/api/v1/share.py
@@ -1,6 +1,7 @@
-from flask import request
+from flask import abort, request
 from flask_restx import Namespace, Resource
 
+from CTFd.utils import get_config
 from CTFd.utils.decorators import authed_only
 from CTFd.utils.social import get_social_share
 from CTFd.utils.user import get_current_user_attrs
@@ -12,6 +13,8 @@ shares_namespace = Namespace("shares", description="Endpoint to create Share lin
 class Share(Resource):
     @authed_only
     def post(self):
+        if bool(get_config("social_shares")) is False:
+            abort(403)
         req = request.get_json()
         share_type = req.get("type")
 
--- CTFd/share.py
+++ CTFd/share.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, abort, request
 
+from CTFd.utils import get_config
 from CTFd.utils.social import get_social_share
 
 social = Blueprint("social", __name__)
@@ -7,6 +8,8 @@ social = Blueprint("social", __name__)
 
 @social.route("/share/<type>/assets/<path>")
 def assets(type, path):
+    if bool(get_config("social_shares")) is False:
+        abort(403)
     SocialShare = get_social_share(type)
     if SocialShare is None:
         abort(404)
@@ -20,6 +23,8 @@ def assets(type, path):
 
 @social.route("/share/<type>")
 def share(type):
+    if bool(get_config("social_shares")) is False:
+        abort(403)
     SocialShare = get_social_share(type)
     if SocialShare is None:
         abort(404)
