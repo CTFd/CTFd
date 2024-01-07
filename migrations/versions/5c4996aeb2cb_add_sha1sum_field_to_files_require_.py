0017c2280602bbe4a62c84fde9a5c7969a76f09d
Kevin Chung <kchung@ctfd.io>
2024-01-07 16:49:22 -0500

    Remove unique constraint on Files location

--- migrations/versions/5c4996aeb2cb_add_sha1sum_field_to_files_require_.py
+++ migrations/versions/5c4996aeb2cb_add_sha1sum_field_to_files_require_.py
@@ -1,4 +1,4 @@
-"""Add sha1sum field to Files, require Files location to be unique
+"""Add sha1sum field to Files
 
 Revision ID: 5c4996aeb2cb
 Revises: 9e6f6578ca84
@@ -17,9 +17,7 @@ depends_on = None
 
 def upgrade():
     op.add_column("files", sa.Column("sha1sum", sa.String(length=40), nullable=True))
-    op.create_unique_constraint(None, "files", ["location"])
 
 
 def downgrade():
-    op.drop_constraint(None, "files", type_="unique")
     op.drop_column("files", "sha1sum")
