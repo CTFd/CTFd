22dc70203bbaf2ae6492aafbebf0a3b120d4293d
Kevin Chung <kchung@ctfd.io>
2024-01-07 13:29:29 -0500

    Add migration for database changes

---
+++ migrations/versions/5c4996aeb2cb_add_sha1sum_field_to_files_require_.py
@@ -0,0 +1,26 @@
+"""Add sha1sum field to Files, require Files location to be unique
+
+Revision ID: 5c4996aeb2cb
+Revises: 9e6f6578ca84
+Create Date: 2024-01-07 13:09:08.843903
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = "5c4996aeb2cb"
+down_revision = "9e6f6578ca84"
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.add_column("files", sa.Column("sha1sum", sa.String(length=40), nullable=True))
+    op.create_unique_constraint(None, "files", ["location"])
+
+
+def downgrade():
+    op.drop_constraint(None, "files", type_="unique")
+    op.drop_column("files", "sha1sum")
