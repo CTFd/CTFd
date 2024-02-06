256f3095b8865878ca4f41926753761e1fafcc4b
Kevin Chung <kchung@ctfd.io>
2024-02-06 00:35:35 -0500

    Test

--- tests/admin/test_pages.py
+++ tests/admin/test_pages.py
@@ -92,5 +92,6 @@ def test_pages_with_link_target():
         with client.session_transaction():
             r = client.get("/")
             html = r.get_data(as_text=True)
+            print(html)
             assert "_blank" in html
     destroy_ctfd(app)
