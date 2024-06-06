d322ee21d0aecc173149370fcea8818112c0ce60
Claire Goeckner-Wald <4743638-cgoecknerwald@users.noreply.gitlab.com>
2024-06-06 11:25:54 +0200

    Standardize BaseChallenge tests

--- tests/challenges/test_base_challenge.py
+++ tests/challenges/test_base_challenge.py
@@ -128,23 +128,6 @@ def test_duplicate_team_mode_solve():
         assert Solves.query.count() == 1
 
 
-def test_none_submission_solve():
-    """
-    Test that passing malformed request to solve() does not add to Solves table
-    """
-    app = create_ctfd()
-    with app.app_context():
-        challenge = gen_challenge(app.db)
-        user = Users.query.filter_by(id=1).first()
-        bad_solve_request = FakeRequest(form={"submission": None})
-
-        r = BaseChallenge.solve(
-            user=user, team=None, challenge=challenge, request=bad_solve_request
-        )
-        assert r == (False, "No submission sent")
-        assert Solves.query.count() == 0
-
-
 def test_incorrect_flag_solve():
     """
     Test that passing an incorrect flag to solve() does adds to Solves table because solve() does not
@@ -175,28 +158,6 @@ def test_incorrect_flag_solve():
     destroy_ctfd(app)
 
 
-def test_empty_request_solve():
-    """
-    Test that passing an empty request to solve() does not add to Solves table
-    """
-    app = create_ctfd()
-    with app.app_context():
-        challenge = gen_challenge(app.db)
-        user = Users.query.filter_by(id=1).first()
-        solve_request = FakeRequest(form={})
-
-        with pytest.raises(
-            Exception, match="'FakeRequest' object has no attribute 'get_json'"
-        ):
-            BaseChallenge.solve(
-                user=user, team=None, challenge=challenge, request=solve_request
-            )
-
-        assert Solves.query.count() == 0
-
-    destroy_ctfd(app)
-
-
 def test_empty_flag_solve():
     """
     Test that passing an empty flag to solve() adds to Solves table
@@ -223,7 +184,46 @@ def test_empty_flag_solve():
     destroy_ctfd(app)
 
 
-def test_correct_attempt():
+def test_none_submission_solve():
+    """
+    Test that passing malformed request to solve() does not add to Solves table
+    """
+    app = create_ctfd()
+    with app.app_context():
+        challenge = gen_challenge(app.db)
+        user = Users.query.filter_by(id=1).first()
+        bad_solve_request = FakeRequest(form={"submission": None})
+
+        r = BaseChallenge.solve(
+            user=user, team=None, challenge=challenge, request=bad_solve_request
+        )
+        assert r == (False, "No submission sent")
+        assert Solves.query.count() == 0
+
+
+def test_empty_form_solve():
+    """
+    Test that passing an empty request to solve() does not add to Solves table
+    """
+    app = create_ctfd()
+    with app.app_context():
+        challenge = gen_challenge(app.db)
+        user = Users.query.filter_by(id=1).first()
+        solve_request = FakeRequest(form={})
+
+        with pytest.raises(
+            Exception, match="'FakeRequest' object has no attribute 'get_json'"
+        ):
+            BaseChallenge.solve(
+                user=user, team=None, challenge=challenge, request=solve_request
+            )
+
+        assert Solves.query.count() == 0
+
+    destroy_ctfd(app)
+
+
+def test_correct_flag_attempt():
     """
     Test that passing a correct flag to attempt() returns (True, "Correct") and
     does not add to the Solves or Fails tables
@@ -242,26 +242,7 @@ def test_correct_attempt():
     destroy_ctfd(app)
 
 
-def test_incorrect_attempt():
-    """
-    Test that passing an incorrect flag to attempt() returns (False, "Incorrect") and
-    does not add to the Solves or Fails tables
-    """
-    app = create_ctfd()
-    with app.app_context():
-        challenge = gen_challenge(app.db)
-        gen_flag(app.db, challenge.id)
-        incorrect_attempt = FakeRequest(form={"submission": "incorrect_flag"})
-
-        r = BaseChallenge.attempt(challenge=challenge, request=incorrect_attempt)
-        assert r == (False, "Incorrect")
-        assert Solves.query.count() == 0
-        assert Fails.query.count() == 0
-
-    destroy_ctfd(app)
-
-
-def test_duplicate_attempt():
+def test_duplicate_correct_attempt():
     """
     Test that passing a duplicate flag to attempt() returns (True, "Correct") and
     does not add to the Solves or Fails tables
@@ -285,7 +266,69 @@ def test_duplicate_attempt():
     destroy_ctfd(app)
 
 
-def test_bad_submission_attempt():
+def test_incorrect_flag_attempt():
+    """
+    Test that passing an incorrect flag to attempt() returns (False, "Incorrect") and
+    does not add to the Solves or Fails tables
+    """
+    app = create_ctfd()
+    with app.app_context():
+        challenge = gen_challenge(app.db)
+        gen_flag(app.db, challenge.id)
+        incorrect_attempt = FakeRequest(form={"submission": "incorrect_flag"})
+
+        r = BaseChallenge.attempt(challenge=challenge, request=incorrect_attempt)
+        assert r == (False, "Incorrect")
+        assert Solves.query.count() == 0
+        assert Fails.query.count() == 0
+
+    destroy_ctfd(app)
+
+
+def test_duplicate_incorrect_attempt():
+    """
+    Test that passing a duplicate incorrect flag to attempt() returns (False, "Incorrect") and
+    does not add to the Solves or Fails tables
+    """
+    app = create_ctfd()
+    with app.app_context():
+        challenge = gen_challenge(app.db)
+        gen_flag(app.db, challenge.id)
+        incorrect_attempt = FakeRequest(form={"submission": "incorrect_flag"})
+
+        r = BaseChallenge.attempt(challenge=challenge, request=incorrect_attempt)
+        assert r == (False, "Incorrect")
+        assert Solves.query.count() == 0
+        assert Fails.query.count() == 0
+
+        r = BaseChallenge.attempt(challenge=challenge, request=incorrect_attempt)
+        assert r == (False, "Incorrect")
+        assert Solves.query.count() == 0
+        assert Fails.query.count() == 0
+
+    destroy_ctfd(app)
+
+
+def test_empty_flag_attempt():
+    """
+    Test that passing an empty flag to attempt() returns (True, "Correct") when the flag matches
+    the challenge's flag and does not add to the Solves or Fails tables
+    """
+    app = create_ctfd()
+    with app.app_context():
+        challenge = gen_challenge(app.db)
+        gen_flag(app.db, challenge.id, content="")
+        empty_flag_attempt = FakeRequest(form={"submission": ""})
+
+        r = BaseChallenge.attempt(challenge=challenge, request=empty_flag_attempt)
+        assert r == (True, "Correct")
+        assert Solves.query.count() == 0
+        assert Fails.query.count() == 0
+
+    destroy_ctfd(app)
+
+
+def test_none_submission_attempt():
     """
     Test that passing a None submission to attempt() returns (False, "No submission sent") and
     does not add to the Solves or Fails tables
@@ -319,25 +362,6 @@ def test_empty_form_attempt():
             Exception, match="'FakeRequest' object has no attribute 'get_json'"
         ):
             BaseChallenge.attempt(challenge=challenge, request=empty_attempt)
-        assert Solves.query.count() == 0
-        assert Fails.query.count() == 0
-
-    destroy_ctfd(app)
-
-
-def test_empty_flag_attempt():
-    """
-    Test that passing an empty flag to attempt() returns (True, "Correct") when the flag matches
-    the challenge's flag and does not add to the Solves or Fails tables
-    """
-    app = create_ctfd()
-    with app.app_context():
-        challenge = gen_challenge(app.db)
-        gen_flag(app.db, challenge.id, content="")
-        empty_flag_attempt = FakeRequest(form={"submission": ""})
-
-        r = BaseChallenge.attempt(challenge=challenge, request=empty_flag_attempt)
-        assert r == (True, "Correct")
         assert Solves.query.count() == 0
         assert Fails.query.count() == 0
 
@@ -397,21 +421,79 @@ def test_good_team_mode_fail():
     destroy_ctfd(app)
 
 
-def test_bad_submission_fail():
+def test_duplicate_user_mode_fail():
     """
-    Test that passing malformed request to fail() does not add to Fails table
+    Test that duplicate fail() in user mode (team=None) adds to Fails table twice,
+    and does not raise an error.
     """
     app = create_ctfd()
     with app.app_context():
         challenge = gen_challenge(app.db)
         user = Users.query.filter_by(id=1).first()
-        bad_fail_request = FakeRequest(form={"submission": None})
+        fail_request = FakeRequest(form={"submission": "flag"})
 
-        r = BaseChallenge.fail(
-            user=user, team=None, challenge=challenge, request=bad_fail_request
+        # First fail attempt
+        BaseChallenge.fail(
+            user=user, team=None, challenge=challenge, request=fail_request
         )
-        assert r == (False, "No submission sent")
-        assert Fails.query.count() == 0
+        assert Fails.query.count() == 1
+        assert (
+            Fails.query.filter_by(
+                user_id=user.id, team_id=None, challenge_id=challenge.id
+            ).count()
+            == 1
+        )
+
+        # Second fail attempt - should not be added to the database
+        BaseChallenge.fail(
+            user=user, team=None, challenge=challenge, request=fail_request
+        )
+        assert Fails.query.count() == 2
+        assert (
+            Fails.query.filter_by(
+                user_id=user.id, team_id=None, challenge_id=challenge.id
+            ).count()
+            == 2
+        )
+
+    destroy_ctfd(app)
+
+
+def test_duplicate_team_mode_fail():
+    """
+    Test that duplicate fail() in team mode (team=not None) adds to Fails table twice,
+    and does not raise an error.
+    """
+    app = create_ctfd()
+    with app.app_context():
+        challenge = gen_challenge(app.db)
+        user = Users.query.filter_by(id=1).first()
+        team = gen_team(app.db)
+        fail_request = FakeRequest(form={"submission": "flag"})
+
+        # First fail attempt
+        BaseChallenge.fail(
+            user=user, team=team, challenge=challenge, request=fail_request
+        )
+        assert Fails.query.count() == 1
+        assert (
+            Fails.query.filter_by(
+                user_id=user.id, team_id=team.id, challenge_id=challenge.id
+            ).count()
+            == 1
+        )
+
+        # Second fail attempt - should not be added to the database
+        BaseChallenge.fail(
+            user=user, team=team, challenge=challenge, request=fail_request
+        )
+        assert Fails.query.count() == 2
+        assert (
+            Fails.query.filter_by(
+                user_id=user.id, team_id=team.id, challenge_id=challenge.id
+            ).count()
+            == 2
+        )
 
     destroy_ctfd(app)
 
@@ -468,5 +550,46 @@ def test_empty_flag_fail():
             ).count()
             == 1
         )
+
+    destroy_ctfd(app)
+
+
+def test_none_submission_fail():
+    """
+    Test that passing malformed request to fail() does not add to Fails table
+    """
+    app = create_ctfd()
+    with app.app_context():
+        challenge = gen_challenge(app.db)
+        user = Users.query.filter_by(id=1).first()
+        bad_fail_request = FakeRequest(form={"submission": None})
+
+        r = BaseChallenge.fail(
+            user=user, team=None, challenge=challenge, request=bad_fail_request
+        )
+        assert r == (False, "No submission sent")
+        assert Fails.query.count() == 0
+
+    destroy_ctfd(app)
+
+
+def test_empty_form_fail():
+    """
+    Test that passing an empty request to fail() does not add to Fails table
+    """
+    app = create_ctfd()
+    with app.app_context():
+        challenge = gen_challenge(app.db)
+        user = Users.query.filter_by(id=1).first()
+        fail_request = FakeRequest(form={})
+
+        with pytest.raises(
+            Exception, match="'FakeRequest' object has no attribute 'get_json'"
+        ):
+            BaseChallenge.fail(
+                user=user, team=None, challenge=challenge, request=fail_request
+            )
+
+        assert Fails.query.count() == 0
 
     destroy_ctfd(app)
