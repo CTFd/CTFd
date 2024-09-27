3c3dc7597c6155d189ed46a817dcd5811daa91c4
Jus-Codin <70018166+Jus-Codin@users.noreply.github.com>
2024-09-27 13:40:19 +0800

    Revert "Check if tables and columns exist before creating/dropping"
    This reverts commit 7cd4c1f5a726b47892e57c7484e5e24767bcfdd1.

--- migrations/versions/9889b8c53673_add_brackets_table.py
+++ migrations/versions/9889b8c53673_add_brackets_table.py
@@ -5,9 +5,8 @@ Revises: 5c4996aeb2cb
 Create Date: 2024-01-25 03:17:52.734753
 
 """
-
-import sqlalchemy as sa
 from alembic import op
+import sqlalchemy as sa
 from sqlalchemy.dialects import mysql
 
 # revision identifiers, used by Alembic.
@@ -18,41 +17,24 @@ depends_on = None
 
 
 def upgrade():
-    # Check if the brackets table exists
-    conn = op.get_bind()
-    inspector = sa.inspect(conn)
-
-    if not inspector.has_table("brackets"):
-        op.create_table(
-            "brackets",
-            sa.Column("id", sa.Integer(), nullable=False),
-            sa.Column("name", sa.String(length=255), nullable=True),
-            sa.Column("description", sa.Text(), nullable=True),
-            sa.Column("type", sa.String(length=80), nullable=True),
-            sa.PrimaryKeyConstraint("id"),
-        )
-
-    team_cols = inspector.get_columns("teams")
-
-    if "bracket_id" not in [col["name"] for col in team_cols]:
-        op.add_column("teams", sa.Column("bracket_id", sa.Integer(), nullable=True))
-        op.create_foreign_key(
-            None, "teams", "brackets", ["bracket_id"], ["id"], ondelete="SET NULL"
-        )
-
-    if "bracket" in [col["name"] for col in team_cols]:
-        op.drop_column("teams", "bracket")
-
-    user_cols = inspector.get_columns("users")
-
-    if "bracket_id" not in [col["name"] for col in user_cols]:
-        op.add_column("users", sa.Column("bracket_id", sa.Integer(), nullable=True))
-        op.create_foreign_key(
-            None, "users", "brackets", ["bracket_id"], ["id"], ondelete="SET NULL"
-        )
-
-    if "bracket" in [col["name"] for col in user_cols]:
-        op.drop_column("users", "bracket")
+    op.create_table(
+        "brackets",
+        sa.Column("id", sa.Integer(), nullable=False),
+        sa.Column("name", sa.String(length=255), nullable=True),
+        sa.Column("description", sa.Text(), nullable=True),
+        sa.Column("type", sa.String(length=80), nullable=True),
+        sa.PrimaryKeyConstraint("id"),
+    )
+    op.add_column("teams", sa.Column("bracket_id", sa.Integer(), nullable=True))
+    op.create_foreign_key(
+        None, "teams", "brackets", ["bracket_id"], ["id"], ondelete="SET NULL"
+    )
+    op.drop_column("teams", "bracket")
+    op.add_column("users", sa.Column("bracket_id", sa.Integer(), nullable=True))
+    op.create_foreign_key(
+        None, "users", "brackets", ["bracket_id"], ["id"], ondelete="SET NULL"
+    )
+    op.drop_column("users", "bracket")
 
 
 def downgrade():
