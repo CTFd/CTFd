23d794186322f542b401e2cf2aab92e0c33028f3
Lua Winkelbach <Kathode-Negative@users.noreply.github.com>
2024-11-21 20:06:20 +0100

    added new user page. working link but wrong style

--- CTFd/plugins/UserChallengeManagement/__init__.py
+++ CTFd/plugins/UserChallengeManagement/__init__.py
@@ -1,5 +1,6 @@
 from flask import render_template
-
+from pathlib import Path
+from CTFd.utils.plugins import override_template
 
 def load(app):
 
@@ -8,11 +9,21 @@ def load(app):
         return render_template('page.html', content="<h1>FAQ Page</h1>")
 
     @app.route('/user/challenges',methods=['GET'])
+    
     def view_challenges():
         return render_template('admin/challenges/challenges.html')
     
+    registerTemplate('users/private.html','newUserPage.html')
+
     #TODO: add custom html extension of admin/challenges/challenges
     #      change methods to check for rights and only display challenges by user
     #      add custom html to change challenge editing to be available to users
     #
     #      add other plugin to modify challenge creation?
+
+
+def registerTemplate(old_path, new_path):
+    dir_path = Path(__file__).parent.resolve()
+    
+    template_path = dir_path/'templates'/new_path
+    override_template(old_path,open(template_path).read())
