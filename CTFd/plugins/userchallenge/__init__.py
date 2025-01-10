6311069a22bfaf599dc2243b275b42570b769bc3
Lua Winkelbach <email(at)email.com>
2025-01-10 10:49:06 +0100

    added checking when accessing challenge not owned by user

--- CTFd/plugins/userchallenge/__init__.py
+++ CTFd/plugins/userchallenge/__init__.py
@@ -73,6 +73,23 @@ def update_allow_challenges():
         db.session.add(conf)
         db.session.commit()
         return True
+def owned_by_user(f):
+    """
+    Decorator that requires the accessed challenge to be registered under the user's name
+    :param f:
+    :return:
+    """
+    @functools.wraps(f)
+    def is_owned_wrapper(*args, **kwargs):
+        user = db.session.query(UserChallenges.user).filter(UserChallenges.challenge == kwargs.get("challenge_id")).first()
+        if (user and get_current_user() and user[0] == get_current_user().id) or is_admin():
+            return f(*args, **kwargs)
+        else:
+            if request.content_type == "application/json":
+                abort(403)
+            else:
+                return redirect(url_for("auth.login", next=request.full_path))
+    return is_owned_wrapper
 
 def userChallenge_allowed(f):
     """
@@ -171,6 +188,7 @@ def load(app):
     
     @userChallenge_allowed
     @app.route('/userchallenge/challenges/<int:challenge_id>',methods=['GET'])
+    @owned_by_user
     @userChallenge_allowed
     def updateChallenge(challenge_id):
         #TODO: update logic to work with plugin   
