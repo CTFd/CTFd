2e8e775be84411f85c0ba0cf4980c6271b4ba4b2
d3vyce <44915747+d3vyce@users.noreply.github.com>
2025-03-02 04:25:21 +0100

    Feat/hint title (#2699)
    * feat: add title to hint
    
    * feat(core-beta): add title to hint
    
    * Remove core-beta change
    
    ---------
    
    Co-authored-by: Kevin Chung <kchung@ctfd.io>

--- CTFd/themes/admin/assets/js/components/hints/HintEditForm.vue
+++ CTFd/themes/admin/assets/js/components/hints/HintEditForm.vue
@@ -26,6 +26,20 @@
                 <div class="col-md-12">
                   <div class="form-group">
                     <label>
+                      Title<br />
+                      <small>Content displayed before hint unlocking</small>
+                    </label>
+                    <input
+                      type="text"
+                      class="form-control"
+                      name="title"
+                      :value="this.title"
+                      ref="title"
+                    />
+                  </div>
+
+                  <div class="form-group">
+                    <label>
                       Hint<br />
                       <small>Markdown &amp; HTML are supported</small>
                     </label>
@@ -110,6 +124,7 @@ export default {
   data: function () {
     return {
       cost: 0,
+      title: null,
       content: null,
       selectedHints: [],
     };
@@ -149,6 +164,7 @@ export default {
           if (response.success) {
             let hint = response.data;
             this.cost = hint.cost;
+            this.title = hint.title;
             this.content = hint.content;
             this.selectedHints = hint.requirements?.prerequisites || [];
             // Wait a little longer because we need the modal to appear.
@@ -175,11 +191,15 @@ export default {
       let editor = this.$refs.content;
       return editor.mde.codemirror.getDoc().getValue();
     },
+    getTitle: function () {
+      return this.$refs.title.value;
+    },
     updateHint: function () {
       let params = {
         challenge_id: this.$props.challenge_id,
         content: this.getContent(),
         cost: this.getCost(),
+        title: getTitle(),
         requirements: { prerequisites: this.selectedHints },
       };
 
