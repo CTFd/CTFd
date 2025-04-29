d1ba6539a9088d1ff6851ff1d8105242ddda9083
Kevin Chung <kchung@ctfd.io>
2025-04-28 23:11:57 -0400

    Free hints now require unlocking before viewing

--- CTFd/api/v1/hints.py
+++ CTFd/api/v1/hints.py
@@ -140,13 +140,6 @@ class Hint(Resource):
             all_unlocks = HintUnlocks.query.filter_by(account_id=user.account_id).all()
             unlock_ids = {unlock.target for unlock in all_unlocks}
 
-            # Get the IDs of all free hints
-            free_hints = Hints.query.filter_by(cost=0).all()
-            free_ids = {h.id for h in free_hints}
-
-            # Add free hints to unlocked IDs
-            unlock_ids.update(free_ids)
-
             # Filter out hint IDs that don't exist
             all_hint_ids = {h.id for h in Hints.query.with_entities(Hints.id).all()}
             prereqs = set(requirements).intersection(all_hint_ids)
@@ -167,14 +160,12 @@ class Hint(Resource):
                     403,
                 )
 
-        view = "unlocked"
-        if hint.cost:
-            view = "locked"
-            unlocked = HintUnlocks.query.filter_by(
-                account_id=user.account_id, target=hint.id
-            ).first()
-            if unlocked:
-                view = "unlocked"
+        view = "locked"
+        unlocked = HintUnlocks.query.filter_by(
+            account_id=user.account_id, target=hint.id
+        ).first()
+        if unlocked:
+            view = "unlocked"
 
         if is_admin():
             if request.args.get("preview", False):
