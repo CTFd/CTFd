048edc4e48fdf612ce69c8c5a44520e893536684
Kevin Chung <kchung@ctfd.io>
2025-06-28 18:04:55 -0400

    Update flags to strip static & regex flags on create/update

--- CTFd/api/v1/flags.py
+++ CTFd/api/v1/flags.py
@@ -96,6 +96,9 @@ class FlagList(Resource):
         if response.errors:
             return {"success": False, "errors": response.errors}, 400
 
+        if response.data.type in ("static", "regex"):
+            response.data.content = response.data.content.strip()
+
         db.session.add(response.data)
         db.session.commit()
 
@@ -182,6 +185,9 @@ class Flag(Resource):
         flag = Flags.query.filter_by(id=flag_id).first_or_404()
         schema = FlagSchema()
         req = request.get_json()
+
+        if flag.type in ("static", "regex"):
+            req["content"] = req["content"].strip()
 
         response = schema.load(req, session=db.session, instance=flag, partial=True)
 
