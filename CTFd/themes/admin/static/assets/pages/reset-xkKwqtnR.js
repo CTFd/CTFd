69dadcf3464377b90d23a90823c8ddc1a21b16a9
Kevin Chung <kchung@ctfd.io>
2025-08-05 20:58:34 -0400

    Run yarn build

---
+++ CTFd/themes/admin/static/assets/pages/reset-xkKwqtnR.js
@@ -0,0 +1,1 @@
+import{$ as e,u as s}from"./main-8It0HuHJ.js";function r(t){t.preventDefault(),s({title:"Reset CTF?",body:"Are you sure you want to reset your CTFd instance?",success:function(){e("#reset-ctf-form").off("submit").submit()}})}e(()=>{e("#reset-ctf-form").submit(r)});
