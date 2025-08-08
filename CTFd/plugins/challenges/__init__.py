bb780892118220f7333da801880ff1bf310f4b0b
Kevin Chung <kchung@ctfd.io>
2025-08-08 02:49:20 -0400

    Move attempt logic into seperate file

--- CTFd/plugins/challenges/__init__.py
+++ CTFd/plugins/challenges/__init__.py
@@ -14,6 +14,11 @@ from CTFd.models import (
     db,
 )
 from CTFd.plugins import register_plugin_assets_directory
+from CTFd.plugins.challenges.logic import (
+    challenge_attempt_all,
+    challenge_attempt_any,
+    challenge_attempt_group,
+)
 from CTFd.plugins.flags import FlagException, get_flag_class
 from CTFd.utils.config import is_teams_mode
 from CTFd.utils.uploads import delete_file
@@ -134,121 +139,11 @@ class BaseChallenge(object):
         flags = Flags.query.filter_by(challenge_id=challenge.id).all()
 
         if challenge.logic == "any":
-            for flag in flags:
-                try:
-                    if get_flag_class(flag.type).compare(flag, submission):
-                        return ChallengeResponse(
-                            status="correct",
-                            message="Correct",
-                        )
-                except FlagException as e:
-                    return ChallengeResponse(
-                        status="incorrect",
-                        message=str(e),
-                    )
-            return ChallengeResponse(
-                status="incorrect",
-                message="Incorrect",
-            )
-
+            return challenge_attempt_any(submission, challenge, flags)
         elif challenge.logic == "all":
-            user = get_current_user()
-            partials = Partials.query.filter_by(
-                account_id=user.account_id, challenge_id=challenge.id
-            ).all()
-            provideds = [partial.provided for partial in partials]
-            provideds.append(submission)
-
-            target_flags_ids = {flag.id for flag in flags}
-            compared_flag_ids = []
-
-            for flag in flags:
-                # Skip flags that we have already evaluated as captured
-                if flag.id in compared_flag_ids:
-                    continue
-                flag_class = get_flag_class(flag.type)
-                for provided in provideds:
-                    if flag_class.compare(flag, provided):
-                        compared_flag_ids.append(flag.id)
-
-            # If we have captured against all flag IDs the challenge is correct
-            if target_flags_ids == set(compared_flag_ids):
-                return ChallengeResponse(
-                    status="correct",
-                    message="Correct",
-                )
-
-            # If we didn't capture all flag IDs we must be missing something.
-            for flag in flags:
-                if get_flag_class(flag.type).compare(flag, submission):
-                    return ChallengeResponse(
-                        status="partial",
-                        message="Correct but more flags are required",
-                    )
-
-            # Input is just wrong
-            return ChallengeResponse(
-                status="incorrect",
-                message="Incorrect",
-            )
-
+            return challenge_attempt_all(submission, challenge, flags)
         elif challenge.logic == "group":
-            if is_teams_mode():
-                user = get_current_user()
-                team = get_current_team()
-                partials = Partials.query.filter_by(
-                    team_id=team.id, challenge_id=challenge.id
-                ).all()
-
-                submitter_ids = {partial.user_id for partial in partials}
-
-                # Check if the user's submission is correct
-                for flag in flags:
-                    try:
-                        if get_flag_class(flag.type).compare(flag, submission):
-                            submitter_ids.add(user.id)
-                            break
-                    except FlagException as e:
-                        return ChallengeResponse(
-                            status="incorrect",
-                            message=str(e),
-                        )
-                else:
-                    return ChallengeResponse(
-                        status="incorrect",
-                        message="Incorrect",
-                    )
-
-                # The submission is correct so compare if we have received from all team members
-                member_ids = {member.id for member in team.members}
-                if member_ids == submitter_ids:
-                    return ChallengeResponse(
-                        status="correct",
-                        message="Correct",
-                    )
-                else:
-                    # We have not received from all members
-                    return ChallengeResponse(
-                        status="partial",
-                        message="Correct but all team members must submit a flag",
-                    )
-            else:
-                for flag in flags:
-                    try:
-                        if get_flag_class(flag.type).compare(flag, submission):
-                            return ChallengeResponse(
-                                status="correct",
-                                message="Correct",
-                            )
-                    except FlagException as e:
-                        return ChallengeResponse(
-                            status="incorrect",
-                            message=str(e),
-                        )
-                return ChallengeResponse(
-                    status="incorrect",
-                    message="Incorrect",
-                )
+            return challenge_attempt_group(submission, challenge, flags)
         else:
             return ChallengeResponse(
                 status="incorrect",
