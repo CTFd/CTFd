61388c3983f0a6d15f84e730bf41bf3f6bdc6233
Joydeep Banik <joydeepbanik41@gmail.com>
2025-11-03 08:10:48 +0000

    Remove unnecessary decorators from challenges listing route

--- CTFd/challenges.py
+++ CTFd/challenges.py
@@ -19,8 +19,7 @@ challenges = Blueprint("challenges", __name__)
 @challenges.route("/challenges", methods=["GET"])
 @require_complete_profile
 @during_ctf_time_only
-@require_verified_emails
-@check_challenge_visibility
+
 def listing():
     if (
         Configs.challenge_visibility == ChallengeVisibilityTypes.PUBLIC
