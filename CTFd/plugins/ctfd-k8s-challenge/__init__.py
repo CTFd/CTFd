88d090b01a686b2d627ec8c982e684144d47a16b
StijnvdMade <stijnmade@gmail.com>
2026-01-03 16:03:09 +0100

    add minimal k8s challenge plugin loader to fix ctfd not booting / responding

---
+++ CTFd/plugins/ctfd-k8s-challenge/__init__.py
@@ -0,0 +1,7 @@
+def load(app):
+    """
+    Minimal k8s challenge plugin loader.
+    This is a placeholder that allows CTFd to start without errors.
+    Actual k8s challenge implementation should be added here.
+    """
+    pass
