f5f30a79109182848de26ae4a1a8479ed0bc5d5c
Felix <fmc@mail.upb.de>
2026-07-31 21:37:14 +0200

    remove unused import

--- CTFd/utils/decorators/__init__.py
+++ CTFd/utils/decorators/__init__.py
@@ -8,7 +8,6 @@ from CTFd.utils import config, get_config
 from CTFd.utils import user as current_user
 from CTFd.utils.config import is_teams_mode
 from CTFd.utils.dates import ctf_ended, ctf_started, ctftime, view_after_ctf
-from CTFd.utils.logging import log
 from CTFd.utils.user import authed, get_current_team, get_current_user, is_admin
 
 
