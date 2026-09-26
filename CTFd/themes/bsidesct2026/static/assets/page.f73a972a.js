d3414acdf1da096c718ed6dac58e676a837f0f6c
Fortyseven <fortyseven@network47.org>
2026-09-25 21:25:03 -0400

    feat: initial 2026 CTFd theme

--- CTFd/themes/bsidesct2026/static/assets/page.f73a972a.js
+++
@@ -1,1 +0,0 @@
-import{C as o,m as d}from"./index.17583699.js";window.CTFd=o;window.Alpine=d;d.start();
---
+++ CTFd/themes/bsidesct2026/static/assets/users_list.750eb491.js
@@ -0,0 +1,1 @@
+import{C as o,m as d}from"./index.5493937a.js";window.CTFd=o;window.Alpine=d;d.start();
